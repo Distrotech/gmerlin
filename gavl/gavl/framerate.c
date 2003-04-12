6ff3117057cac013640f5f5b31cffa96d2b44837
gmerlin <gmerlin@214d8ac5-8513-4f59-80f8-2f39b674a24a>
2003-04-12 17:24:49 +0000

    * Simplyfied some stuff
    * Added simple video framerate converter (based on repeating/dropping frames)
    
    * Video framerate is double now
    
    
    git-svn-id: svn://svn.code.sf.net/p/gmerlin/code/trunk@11 214d8ac5-8513-4f59-80f8-2f39b674a24a

---
+++ gavl/gavl/framerate.c
@@ -0,0 +1,126 @@
+#include <stdlib.h>
+
+#include <gavl.h>
+
+struct gavl_framerate_converter_s
+  {
+  gavl_time_t input_time_1;
+  gavl_time_t input_time_2;
+  gavl_time_t next_output_time;
+
+  gavl_video_frame_t * input_frame_1;
+  gavl_video_frame_t * input_frame_2;
+  
+  double output_framerate;
+  int64_t output_frame_count;
+  int do_resync;
+  };
+
+gavl_framerate_converter_t * gavl_framerate_converter_create()
+  {
+  gavl_framerate_converter_t * ret;
+  ret = calloc(1, sizeof(*ret));
+  return ret;
+  }
+
+void
+gavl_framerate_converter_init(gavl_framerate_converter_t * cnv,
+                              double output_framerate)
+  {
+  cnv->output_framerate = output_framerate;
+  cnv->output_frame_count = 0;
+  cnv->do_resync = 1;
+  }
+
+void gavl_framerate_converter_reset(gavl_framerate_converter_t * cnv)
+  {
+  cnv->input_frame_1 = (gavl_video_frame_t*)0;
+  cnv->input_frame_2 = (gavl_video_frame_t*)0;
+  cnv->do_resync = 1;
+  }
+
+/*
+ * Convert framerate:
+ * input are 2 subsequent frames with valid timestamps
+ * output is one of the input frames with corrected timestamps
+ *
+ * The timestamps of the input frames should be considered
+ * undefined afterwards
+ */
+
+/*
+#define GAVL_FRAMERATE_INPUT_1_DONE (1<<0)
+#define GAVL_FRAMERATE_INPUT_2_DONE (1<<1)
+#define GAVL_FRAMERATE_OUTPUT_DONE  (1<<2)
+*/
+
+
+int gavl_framerate_convert(gavl_framerate_converter_t * cnv,
+                           gavl_video_frame_t * input_frame_1,
+                           gavl_video_frame_t * input_frame_2,
+                           gavl_video_frame_t ** output_frame)
+  {
+  int ret = 0;
+
+  /* Correct times */
+
+  if(cnv->input_frame_1 != input_frame_1)
+    {
+    cnv->input_frame_1 = input_frame_1;
+    cnv->input_time_1 = cnv->input_frame_1->time;
+    }
+  if(cnv->input_frame_2 != input_frame_2)
+    {
+    cnv->input_frame_2 = input_frame_2;
+    cnv->input_time_2 = cnv->input_frame_2->time;
+    }
+
+  if(cnv->do_resync)
+    {
+    gavl_time_to_frames(&(cnv->output_frame_count),
+                        cnv->output_framerate,
+                        cnv->input_time_1);
+    gavl_frames_to_time(&(cnv->next_output_time),
+                        cnv->output_framerate,
+                        cnv->output_frame_count);
+    cnv->do_resync = 0;
+    }
+
+  if(cnv->next_output_time > cnv->input_time_2)
+    {
+    ret |= GAVL_FRAMERATE_INPUT_DONE;
+    }
+  /* Ill conditioned case: Repeat first frame, until
+     our time reaches timestamp of first first frame */
+  
+  else if(cnv->next_output_time < cnv->input_time_1)
+    {
+    *output_frame = input_frame_1;
+    (*output_frame)->time = cnv->next_output_time;
+    ret |= GAVL_FRAMERATE_OUTPUT_DONE;
+    }
+  /* Our time is between the input time, take the nearest frame */
+  else
+    {
+    if(cnv->next_output_time - cnv->input_time_1 <
+       cnv->input_time_2 - cnv->next_output_time)
+      *output_frame = input_frame_1;
+    else
+      *output_frame = input_frame_1;
+    (*output_frame)->time = cnv->next_output_time;
+    ret |= GAVL_FRAMERATE_OUTPUT_DONE;
+    }
+  
+  cnv->output_frame_count++;
+
+  gavl_frames_to_time(&(cnv->next_output_time),
+                      cnv->output_framerate,
+                      cnv->output_frame_count);
+  return ret;
+
+  }
+
+void gavl_framerate_converter_destroy(gavl_framerate_converter_t * cnv)
+  {
+  free(cnv);
+  }
