d62a9abe8be1a0e08cb75c892a9f78d752e27a17
gmerlin <gmerlin@214d8ac5-8513-4f59-80f8-2f39b674a24a>
2003-08-09 11:23:20 +0000

    * Forgot this file
    git-svn-id: svn://svn.code.sf.net/p/gmerlin/code/trunk@17 214d8ac5-8513-4f59-80f8-2f39b674a24a

---
+++ gavl/gavl/videoformat.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <gavl/gavl.h>
+
+void gavl_video_format_dump(gavl_video_format_t * format)
+  {
+  fprintf(stderr, "Frame size:   %d x %d\n",
+          format->frame_width, format->frame_height);
+  fprintf(stderr, "Image size:   %d x %d\n",
+          format->image_width, format->image_height);
+  fprintf(stderr, "Pixel size:   %d x %d\n",
+          format->pixel_width, format->pixel_height);
+  fprintf(stderr, "Pixel format: %s\n",
+          gavl_colorspace_to_string(format->colorspace));
+  fprintf(stderr, "Framerate:    %f",
+          (float)(format->framerate_num)/((float)format->framerate_den));
+
+  if(format->framerate_den != 1)
+    fprintf(stderr, " [%d / %d]", format->framerate_num,
+            format->framerate_den);
+  fprintf(stderr, " fps");
+
+  if(!format->free_framerate)
+    fprintf(stderr, " (Constant)\n");
+  else
+    fprintf(stderr, " (Not constant)\n");
+  }
