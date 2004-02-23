afd7c0025b0eaca314737c7326f33268c0259412
gmerlin <gmerlin@214d8ac5-8513-4f59-80f8-2f39b674a24a>
2004-02-23 19:16:07 +0000

    * libmpeg2 and liba52 based decoders
    git-svn-id: svn://svn.code.sf.net/p/gmerlin/code/trunk@55 214d8ac5-8513-4f59-80f8-2f39b674a24a

---
+++ gmerlin_avdecoder/lib/audio_a52.c
@@ -0,0 +1,501 @@
+/*****************************************************************
+ 
+  audio_a52.c
+ 
+  Copyright (c) 2003-2004 by Burkhard Plaum - plaum@ipf.uni-stuttgart.de
+ 
+  http://gmerlin.sourceforge.net
+ 
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+ 
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111, USA.
+ 
+*****************************************************************/
+
+#include <stdlib.h>
+#include <string.h>
+
+#include <avdec_private.h>
+#include <a52dec/a52.h>
+
+#define FRAME_SAMPLES 1536
+
+typedef struct
+  {
+  int total_bytes;
+  int samplerate;
+  int bitrate;
+
+  int acmod;
+  int lfe;
+  int dolby;
+
+  float cmixlev;
+  float smixlev;
+  sample_t * samples;
+  
+  } a52_header;
+
+typedef struct
+  {
+  a52_header header;
+  
+  a52_state_t * state;
+  sample_t    * samples;
+
+  uint8_t * buffer;
+  int bytes_in_buffer;
+  
+  bgav_packet_t * packet;
+  uint8_t       * packet_ptr;
+  gavl_audio_frame_t * frame;
+  } a52_priv;
+
+#define MAX_FRAME_SIZE 3840
+#define HEADER_BYTES 7
+
+#define LEVEL_3DB 0.7071067811865476
+#define LEVEL_45DB 0.5946035575013605
+#define LEVEL_6DB 0.5
+
+static uint8_t halfrate[12] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 2, 3};
+static int rate[] = { 32,  40,  48,  56,  64,  80,  96, 112,
+                      128, 160, 192, 224, 256, 320, 384, 448,
+                      512, 576, 640};
+static uint8_t lfeon[8] = {0x10, 0x10, 0x04, 0x04, 0x04, 0x01, 0x04, 0x01};
+
+static float clev[4] = {LEVEL_3DB, LEVEL_45DB, LEVEL_6DB, LEVEL_45DB};
+static float slev[4] = {LEVEL_3DB, LEVEL_6DB,          0, LEVEL_6DB};
+
+static int a52_header_read(a52_header * ret, uint8_t * buf)
+  {
+  int half;
+  int frmsizecod;
+  int bitrate;
+
+  int cmixlev;
+  int smixlev;
+  
+  
+  if ((buf[0] != 0x0b) || (buf[1] != 0x77))   /* syncword */
+    return 0;
+
+  if (buf[5] >= 0x60)         /* bsid >= 12 */
+    return 0;
+
+  half = halfrate[buf[5] >> 3];
+
+  /* acmod, dsurmod and lfeon */
+  ret->acmod = buf[6] >> 5;
+
+  /* cmixlev and surmixlev */
+
+  if((ret->acmod & 0x01) && (ret->acmod != 0x01))
+    {
+    cmixlev = (buf[6] & 0x18) >> 3;
+    }
+  else
+    cmixlev = -1;
+  
+  if(ret->acmod & 0x04)
+    {
+    if((cmixlev) == -1)
+      smixlev = (buf[6] & 0x18) >> 3;
+    else
+      smixlev = (buf[6] & 0x06) >> 1;
+    }
+  else
+    smixlev = -1;
+
+  if(smixlev >= 0)
+    ret->smixlev = slev[smixlev];
+  if(cmixlev >= 0)
+    ret->cmixlev = clev[cmixlev];
+  
+  if((buf[6] & 0xf8) == 0x50)
+    ret->dolby = 1;
+
+  if(buf[6] & lfeon[ret->acmod])
+    ret->lfe = 1;
+
+  frmsizecod = buf[4] & 63;
+  if (frmsizecod >= 38)
+    return 0;
+  bitrate = rate[frmsizecod >> 1];
+  ret->bitrate = (bitrate * 1000) >> half;
+
+  switch (buf[4] & 0xc0)
+    {
+    case 0:
+      ret->samplerate = 48000 >> half;
+      ret->total_bytes = 4 * bitrate;
+      break;
+    case 0x40:
+      ret->samplerate = 44100 >> half;
+      ret->total_bytes =  2 * (320 * bitrate / 147 + (frmsizecod & 1));
+      break;
+    case 0x80:
+      ret->samplerate = 32000 >> half;
+      ret->total_bytes =  6 * bitrate;
+      break;
+    default:
+      return 0;
+    }
+  
+  return 1;
+  }
+
+static void a52_header_dump(a52_header * h)
+  {
+  fprintf(stderr, "A52 header:\n");
+  fprintf(stderr, "  Frame bytes: %d\n", h->total_bytes);
+  fprintf(stderr, "  Samplerate:  %d\n", h->samplerate);
+  fprintf(stderr, "  acmod:  0x%0x\n",   h->acmod);
+  if(h->smixlev >= 0.0)
+    fprintf(stderr, "  smixlev: %f\n", h->smixlev);
+  if(h->cmixlev >= 0.0)
+    fprintf(stderr, "  cmixlev: %f\n", h->cmixlev);
+  }
+
+/*
+ *  Parse header and read data bytes for that frame
+ *  (also does resync)
+ */
+
+static int get_data(bgav_stream_t * s, int num_bytes)
+  {
+  int bytes_to_copy;
+  a52_priv * priv;
+  priv = (a52_priv*)(s->data.audio.decoder->priv);
+  
+  while(priv->bytes_in_buffer < num_bytes)
+    {
+    if(!priv->packet)
+      {
+      priv->packet = bgav_demuxer_get_packet_read(s->demuxer, s);
+      if(!priv->packet)
+        return 0;
+      priv->packet_ptr = priv->packet->data;
+      }
+    else if(priv->packet_ptr - priv->packet->data >= priv->packet->data_size)
+      {
+      bgav_demuxer_done_packet_read(s->demuxer, priv->packet);
+      priv->packet = bgav_demuxer_get_packet_read(s->demuxer, s);
+      if(!priv->packet)
+        return 0;
+      priv->packet_ptr = priv->packet->data;
+      }
+    bytes_to_copy = num_bytes - priv->bytes_in_buffer;
+    
+    if(bytes_to_copy >
+       priv->packet->data_size - (priv->packet_ptr - priv->packet->data))
+      {
+      bytes_to_copy =
+        priv->packet->data_size - (priv->packet_ptr - priv->packet->data);
+      }
+    memcpy(priv->buffer + priv->bytes_in_buffer,
+           priv->packet_ptr, bytes_to_copy);
+    priv->bytes_in_buffer += bytes_to_copy;
+    priv->packet_ptr      += bytes_to_copy;
+    }
+
+  return 1;
+  }
+
+static void done_data(bgav_stream_t * s, int num_bytes)
+  {
+  a52_priv * priv;
+  int bytes_left;
+  
+  priv = (a52_priv*)(s->data.audio.decoder->priv);
+
+  bytes_left = priv->bytes_in_buffer - num_bytes;
+  
+  if(bytes_left < 0)
+    {
+    fprintf(stderr, "BUUUUUG in liba52!!!\n");
+    return;
+    }
+  else if(bytes_left > 0)
+    {
+    memmove(priv->buffer,
+            priv->buffer + num_bytes,
+            bytes_left);
+    }
+  priv->bytes_in_buffer -= num_bytes;
+  }
+
+static int do_resync(bgav_stream_t * s)
+  {
+  a52_priv * priv;
+  priv = (a52_priv*)(s->data.audio.decoder->priv);
+
+  while(1)
+    {
+    if(!get_data(s, HEADER_BYTES))
+      return 0;
+    if(a52_header_read(&(priv->header), priv->buffer))
+      return 1;
+    done_data(s, 1);
+    }
+  return 0;
+  }
+
+static void resync_a52(bgav_stream_t * s)
+  {
+  a52_priv * priv;
+  priv = (a52_priv*)(s->data.audio.decoder->priv);
+
+  priv->packet = (bgav_packet_t*)0;
+  priv->packet_ptr = (uint8_t*)0;
+  priv->bytes_in_buffer = 0;
+  do_resync(s);
+  }
+
+static int init_a52(bgav_stream_t * s)
+  {
+  a52_priv * priv;
+  
+  priv = calloc(1, sizeof(*priv));
+  priv->buffer = calloc(MAX_FRAME_SIZE, 1);
+  s->data.audio.decoder->priv = priv;
+  do_resync(s);
+  
+  a52_header_dump(&(priv->header));
+
+  /* Get format */
+
+  s->data.audio.format.samplerate = priv->header.samplerate;
+  s->codec_bitrate = priv->header.bitrate;
+  s->data.audio.format.samples_per_frame = FRAME_SAMPLES;
+  s->data.audio.format.sample_format = GAVL_SAMPLE_FLOAT;
+  
+  if(priv->header.lfe)
+    {
+    s->data.audio.format.lfe = 1;
+    s->data.audio.format.num_channels = 1;
+    s->data.audio.format.channel_locations[0] = GAVL_CHID_LFE;
+    }
+  else
+    s->data.audio.format.num_channels = 0;
+  
+  switch(priv->header.acmod)
+    {
+    case A52_CHANNEL:
+    case A52_STEREO:
+      s->data.audio.format.channel_setup = GAVL_CHANNEL_STEREO;
+      
+      s->data.audio.format.channel_locations[s->data.audio.format.num_channels] = 
+        GAVL_CHID_FRONT_LEFT;
+      s->data.audio.format.channel_locations[s->data.audio.format.num_channels+1] = 
+        GAVL_CHID_FRONT_RIGHT;
+      s->data.audio.format.num_channels += 2;
+      break;
+    case A52_MONO:
+      s->data.audio.format.channel_setup = GAVL_CHANNEL_MONO;
+      s->data.audio.format.channel_locations[s->data.audio.format.num_channels] = 
+        GAVL_CHID_FRONT;
+      s->data.audio.format.num_channels += 1;
+      break;
+    case A52_3F:
+      s->data.audio.format.channel_setup = GAVL_CHANNEL_3F;
+      s->data.audio.format.channel_locations[s->data.audio.format.num_channels] = 
+        GAVL_CHID_FRONT_LEFT;
+      s->data.audio.format.channel_locations[s->data.audio.format.num_channels+1] = 
+        GAVL_CHID_FRONT_CENTER;
+      s->data.audio.format.channel_locations[s->data.audio.format.num_channels+2] = 
+        GAVL_CHID_FRONT_RIGHT;
+      s->data.audio.format.num_channels += 3;
+      break;
+    case A52_2F1R:
+      s->data.audio.format.channel_setup = GAVL_CHANNEL_2F1R;
+      s->data.audio.format.channel_locations[s->data.audio.format.num_channels] = 
+        GAVL_CHID_FRONT_LEFT;
+      s->data.audio.format.channel_locations[s->data.audio.format.num_channels+1] = 
+        GAVL_CHID_FRONT_RIGHT;
+      s->data.audio.format.channel_locations[s->data.audio.format.num_channels+2] = 
+        GAVL_CHID_REAR;
+      s->data.audio.format.num_channels += 3;
+      break;
+    case A52_3F1R:
+      s->data.audio.format.channel_setup = GAVL_CHANNEL_3F1R;
+      s->data.audio.format.channel_locations[s->data.audio.format.num_channels] = 
+        GAVL_CHID_FRONT_LEFT;
+      s->data.audio.format.channel_locations[s->data.audio.format.num_channels+1] = 
+        GAVL_CHID_FRONT_CENTER;
+      s->data.audio.format.channel_locations[s->data.audio.format.num_channels+2] = 
+        GAVL_CHID_FRONT_RIGHT;
+      s->data.audio.format.channel_locations[s->data.audio.format.num_channels+3] = 
+        GAVL_CHID_REAR;
+      s->data.audio.format.num_channels += 4;
+
+      break;
+    case A52_2F2R:
+      s->data.audio.format.channel_setup = GAVL_CHANNEL_2F2R;
+      s->data.audio.format.channel_locations[s->data.audio.format.num_channels] = 
+        GAVL_CHID_FRONT_LEFT;
+      s->data.audio.format.channel_locations[s->data.audio.format.num_channels+1] = 
+        GAVL_CHID_FRONT_RIGHT;
+      s->data.audio.format.channel_locations[s->data.audio.format.num_channels+2] = 
+        GAVL_CHID_REAR_LEFT;
+      s->data.audio.format.channel_locations[s->data.audio.format.num_channels+3] = 
+        GAVL_CHID_REAR_RIGHT;
+      s->data.audio.format.num_channels += 4;
+      break;
+    case A52_3F2R:
+      s->data.audio.format.channel_setup = GAVL_CHANNEL_3F2R;
+      s->data.audio.format.channel_locations[s->data.audio.format.num_channels] = 
+        GAVL_CHID_FRONT_LEFT;
+      s->data.audio.format.channel_locations[s->data.audio.format.num_channels+1] = 
+        GAVL_CHID_FRONT_CENTER;
+      s->data.audio.format.channel_locations[s->data.audio.format.num_channels+2] = 
+        GAVL_CHID_FRONT_RIGHT;
+      s->data.audio.format.channel_locations[s->data.audio.format.num_channels+3] = 
+        GAVL_CHID_REAR_LEFT;
+      s->data.audio.format.channel_locations[s->data.audio.format.num_channels+4] = 
+        GAVL_CHID_REAR_RIGHT;
+      s->data.audio.format.num_channels += 5;
+      break;
+    }
+
+  if(gavl_front_channels(&(s->data.audio.format)) == 3)
+    s->data.audio.format.center_level = priv->header.cmixlev;
+  if(gavl_rear_channels(&(s->data.audio.format)))
+    s->data.audio.format.rear_level = priv->header.smixlev;
+
+  priv->frame = gavl_audio_frame_create(&(s->data.audio.format));
+  priv->state = a52_init(0);
+  priv->samples = a52_samples(priv->state);
+  s->description =
+    bgav_sprintf("AC3 %d kb/sec", priv->header.bitrate/1000);
+  
+  return 1;
+  }
+
+static int decode_frame(bgav_stream_t * s)
+  {
+  int flags;
+  int sample_rate;
+  int bit_rate;
+  int i, j;
+#ifdef LIBA52_DOUBLE
+  int k;
+#endif
+  
+  sample_t level = 1.0;
+  
+  a52_priv * priv;
+  priv = (a52_priv*)s->data.audio.decoder->priv;
+
+  //  fprintf(stderr, "Decode frame");
+  
+  if(!do_resync(s))
+    return 0;
+  if(!get_data(s, priv->header.total_bytes))
+    return 0;
+
+  /* Now, decode this */
+  
+  if(!a52_syncinfo(priv->buffer, &flags,
+                   &sample_rate, &bit_rate))
+    return 0;
+
+  a52_frame(priv->state, priv->buffer, &flags,
+            &level, 0.0);
+  a52_dynrng(priv->state, NULL, NULL);
+
+  for(i = 0; i < 6; i++)
+    {
+    a52_block (priv->state);
+
+    for(j = 0; j < s->data.audio.format.num_channels; j++)
+      {
+#ifdef LIBA52_DOUBLE
+      for(k = 0; k < 256; k++)
+        priv->frame->channels.f[i][k][i*256] = priv->samples[j*256 + k];
+        
+#else
+      memcpy(&(priv->frame->channels.f[j][i*256]),
+             &(priv->samples[j * 256]),
+             256 * sizeof(float));
+#endif
+      }
+    }
+  done_data(s, priv->header.total_bytes);
+  
+  priv->frame->valid_samples = FRAME_SAMPLES;
+  return 1;
+  }
+
+static int decode_a52(bgav_stream_t * s,
+                      gavl_audio_frame_t * f, int num_samples)
+  {
+  int samples_decoded = 0;
+  int samples_copied;
+  a52_priv * priv;
+  priv = (a52_priv*)s->data.audio.decoder->priv;
+
+  while(samples_decoded < num_samples)
+    {
+    if(!priv->frame->valid_samples)
+      {
+      if(!decode_frame(s))
+        {
+        if(f)
+          f->valid_samples = samples_decoded;
+        fprintf(stderr, "A52: EOF\n");
+        return samples_decoded;
+        }
+      }
+    samples_copied =
+      gavl_audio_frame_copy(&(s->data.audio.format),
+                            f,
+                            priv->frame,
+                            samples_decoded, /* out_pos */
+                            FRAME_SAMPLES -
+                            priv->frame->valid_samples,  /* in_pos */
+                            num_samples - samples_decoded, /* out_size, */
+                            priv->frame->valid_samples /* in_size */);
+    priv->frame->valid_samples -= samples_copied;
+    samples_decoded += samples_copied;
+    }
+  if(f)
+    {
+    f->valid_samples = samples_decoded;
+    }
+  //  fprintf(stderr, "done %d\n", samples_decoded);
+  return samples_decoded;
+  }
+
+static void close_a52(bgav_stream_t * s)
+  {
+  a52_priv * priv;
+  priv = (a52_priv*)s->data.audio.decoder->priv;
+
+  if(priv->frame)
+    gavl_audio_frame_destroy(priv->frame);
+  a52_free(priv->state);
+  free(priv);
+  }
+
+static bgav_audio_decoder_t decoder =
+  {
+    fourccs: (int[]){ BGAV_WAVID_2_FOURCC(0x2000),
+                      BGAV_MK_FOURCC('.', 'a', 'c', '3'), 0x00 },
+    name: "liba52 based decoder",
+
+    init:   init_a52,
+    decode: decode_a52,
+    close:  close_a52,
+    resync: resync_a52,
+  };
+
+void bgav_init_audio_decoders_a52()
+  {
+  bgav_audio_decoder_register(&decoder);
+  }
---
+++ gmerlin_avdecoder/lib/video_libmpeg2.c
@@ -0,0 +1,302 @@
+/*****************************************************************
+ 
+  video_libmpeg2.c
+ 
+  Copyright (c) 2003-2004 by Burkhard Plaum - plaum@ipf.uni-stuttgart.de
+ 
+  http://gmerlin.sourceforge.net
+ 
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+ 
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111, USA.
+ 
+*****************************************************************/
+
+#include <avdec_private.h>
+#include <stdlib.h>
+#include <mpeg2dec/mpeg2.h>
+
+typedef struct
+  {
+  const mpeg2_info_t * info;
+  mpeg2dec_t * dec;
+  gavl_video_frame_t * frame;
+  bgav_packet_t * p;
+  
+  gavl_time_t picture_duration;
+  gavl_time_t picture_timestamp;
+
+  int intra_slice_refresh;
+  } mpeg2_priv_t;
+
+static int get_data(bgav_stream_t*s)
+  {
+  mpeg2_priv_t * priv;
+  priv = (mpeg2_priv_t*)(s->data.video.decoder->priv);
+  
+  if(priv->p)
+    {
+    bgav_demuxer_done_packet_read(s->demuxer, priv->p);
+    }
+  priv->p = bgav_demuxer_get_packet_read(s->demuxer, s);
+  if(!priv->p)
+    return 0;
+  
+  mpeg2_buffer(priv->dec, priv->p->data, priv->p->data + priv->p->data_size);
+  
+  if(priv->p->timestamp >= 0)
+    {
+    //    fprintf(stderr, "Got pts %f\n", gavl_time_to_seconds(priv->p->timestamp));
+    mpeg2_tag_picture(priv->dec,
+                      (priv->p->timestamp) >> 32,
+                      priv->p->timestamp & 0xffffffff);
+    }
+
+  return 1;
+  }
+
+static int parse(bgav_stream_t*s, mpeg2_state_t * state)
+  {
+  mpeg2_priv_t * priv;
+  priv = (mpeg2_priv_t*)(s->data.video.decoder->priv);
+
+  while(1)
+    {
+    *state = mpeg2_parse(priv->dec);
+    if(*state == STATE_BUFFER)
+      {
+      if(!get_data(s))
+        return 0;
+      }
+    else
+      return 1;
+    }
+  }
+
+static void get_format(gavl_video_format_t * ret,
+                       const mpeg2_sequence_t * sequence)
+  {
+  switch(sequence->frame_period)
+    {
+    case 1126125:
+      ret->framerate_num = 24000;
+      ret->framerate_den = 1001;
+      break;
+    case 1125000:
+      ret->framerate_num = 24;
+      ret->framerate_den = 1;
+      break;
+    case 1080000:
+      ret->framerate_num = 25;
+      ret->framerate_den = 1;
+      break;
+    case 900900:
+      ret->framerate_num = 30000;
+      ret->framerate_den = 1001;
+      break;
+    case 900000:
+      ret->framerate_num = 30;
+      ret->framerate_den = 1;
+      break;
+    case 540000:
+      ret->framerate_num = 50;
+      ret->framerate_den = 1;
+      break;
+    case 450450:
+      ret->framerate_num = 60000;
+      ret->framerate_den = 1001;
+    case 450000:
+      ret->framerate_num = 60;
+      ret->framerate_den = 1;
+      break;
+    }
+
+  ret->image_width  = sequence->picture_width;
+  ret->image_height = sequence->picture_height;
+                                                                               
+  ret->frame_width  = sequence->width;
+  ret->frame_height = sequence->height;
+                                                                               
+  ret->pixel_width  = sequence->pixel_width;
+  ret->pixel_height = sequence->pixel_height;
+
+  if(sequence->chroma_height == sequence->height/2)
+    ret->colorspace = GAVL_YUV_420_P;
+  else if(sequence->chroma_height == sequence->height)
+    ret->colorspace = GAVL_YUV_422_P;
+  
+  if(sequence->flags & SEQ_FLAG_MPEG2)
+    ret->free_framerate = 1;
+  }
+
+int init_mpeg2(bgav_stream_t*s)
+  {
+  mpeg2_state_t state;
+  mpeg2_priv_t * priv;
+
+  priv = calloc(1, sizeof(*priv));
+  s->data.video.decoder->priv = priv;
+  
+  priv->dec  = mpeg2_init();
+  priv->info = mpeg2_info(priv->dec);
+  
+  while(1)
+    {
+    if(!parse(s, &state))
+      return 0;
+    if(state == STATE_SEQUENCE)
+      break;
+    }
+  
+  /* Get format */
+  
+  get_format(&(s->data.video.format), priv->info->sequence);
+  priv->frame = gavl_video_frame_create(NULL);
+
+  priv->frame->strides[0] = priv->info->sequence->width;
+  priv->frame->strides[1] = priv->info->sequence->chroma_width;
+  priv->frame->strides[2] = priv->info->sequence->chroma_width;
+  
+  s->description =
+    bgav_sprintf("MPEG-%d, %.2f kb/sec", 
+                 (priv->info->sequence->flags & SEQ_FLAG_MPEG2) ? 2 : 1,
+                 (float)priv->info->sequence->byte_rate * 8.0 / 1000.0);
+  s->codec_bitrate = priv->info->sequence->byte_rate * 8;
+  
+  while(1)
+    {
+    if(!parse(s, &state))
+      return 0;
+    if(state == STATE_PICTURE)
+      break;
+    }
+  /*
+   *  If the first frame after a sequence header is a P-Frame, we have most likely an
+   *  intra slice refresh stream
+   */
+
+  if(priv->info->current_picture->flags & PIC_FLAG_CODING_TYPE_P)
+    priv->intra_slice_refresh = 1;
+  
+  return 1;
+  }
+
+int decode_mpeg2(bgav_stream_t*s, gavl_video_frame_t*f)
+  {
+  mpeg2_priv_t * priv;
+  mpeg2_state_t state;
+  priv = (mpeg2_priv_t*)(s->data.video.decoder->priv);
+  /* Decode frame */
+
+  if(f)
+    mpeg2_skip(priv->dec, 0);
+  else
+    mpeg2_skip(priv->dec, 1);
+
+  while(1)
+    {
+    if(!parse(s, &state))
+      return 0;
+    if((state == STATE_END) || (state == STATE_SLICE) ||
+       (state == STATE_INVALID_END))
+      break;
+    }
+  
+  /* Copy frame */
+
+  if(priv->info->display_fbuf)
+    {
+    /* Calculate timestamp */
+    
+    if(priv->info->display_picture->flags & PIC_FLAG_TAGS)
+      {
+      priv->picture_timestamp = priv->info->display_picture->tag;
+      priv->picture_timestamp <<= 32;
+      priv->picture_timestamp |= priv->info->display_picture->tag2;
+      }
+    else
+      {
+      priv->picture_timestamp += priv->picture_duration;
+      }
+    /* Get this pictures duration */
+
+    priv->picture_duration =
+      (GAVL_TIME_SCALE * s->data.video.format.framerate_den) /
+      s->data.video.format.framerate_num;
+    
+    if((priv->info->display_picture->flags & PIC_FLAG_TOP_FIELD_FIRST) &&
+       (priv->info->display_picture->nb_fields > 2))
+      {
+      priv->picture_duration =
+        (priv->picture_duration * priv->info->current_picture->nb_fields) / 2;
+      }
+    
+    if(f)
+      {
+      priv->frame->planes[0] = priv->info->display_fbuf->buf[0];
+      priv->frame->planes[1] = priv->info->display_fbuf->buf[1];
+      priv->frame->planes[2] = priv->info->display_fbuf->buf[2];
+      gavl_video_frame_copy(&(s->data.video.format), f, priv->frame);
+      }
+    s->time = priv->picture_timestamp;
+    }
+  //  fprintf(stderr, "Timestamp: %f\n",
+  //          gavl_time_to_seconds(priv->picture_timestamp));
+  
+  return 1;
+  }
+
+void resync_mpeg2(bgav_stream_t*s)
+  {
+  mpeg2_state_t state;
+  mpeg2_priv_t * priv;
+  priv = (mpeg2_priv_t*)(s->data.video.decoder->priv);
+  mpeg2_reset(priv->dec, 0);
+  mpeg2_buffer(priv->dec, NULL, NULL);
+  mpeg2_skip(priv->dec, 1);
+  while(1)
+    {
+    while(1)
+      {
+      if(!parse(s, &state))
+        return;
+      if(state == STATE_PICTURE)
+        break;
+      }
+    if((priv->intra_slice_refresh)  &&
+       (priv->info->current_picture->flags & PIC_FLAG_CODING_TYPE_P))
+      break;
+    else if(priv->info->current_picture->flags & PIC_FLAG_CODING_TYPE_I)
+      break;
+    }
+  mpeg2_skip(priv->dec, 0);
+  }
+
+  
+void close_mpeg2(bgav_stream_t*s)
+  {
+  mpeg2_priv_t * priv;
+  priv = (mpeg2_priv_t*)(s->data.video.decoder->priv);
+  
+  }
+
+static bgav_video_decoder_t decoder =
+  {
+    fourccs: (uint32_t[]){ BGAV_MK_FOURCC('m','p','g','v'), 0x00 },
+    name:    "libmpeg2 decoder",
+
+    init:    init_mpeg2,
+    decode:  decode_mpeg2,
+    close:   close_mpeg2,
+    resync:  resync_mpeg2,
+  };
+
+void bgav_init_video_decoders_libmpeg2()
+  {
+  bgav_video_decoder_register(&decoder);
+  }
