23d5a54b7fc93191b117042e611ba45c17995de2
gmerlin <gmerlin@214d8ac5-8513-4f59-80f8-2f39b674a24a>
2004-02-23 19:27:46 +0000

    * Added Misrosoft style [reference] redirector
    git-svn-id: svn://svn.code.sf.net/p/gmerlin/code/trunk@58 214d8ac5-8513-4f59-80f8-2f39b674a24a

---
+++ gmerlin_avdecoder/lib/r_ref.c
@@ -0,0 +1,79 @@
+/*****************************************************************
+ 
+  r_ref.c
+ 
+  Copyright (c) 2003-2004 by Burkhard Plaum - plaum@ipf.uni-stuttgart.de
+ 
+  http://gmerlin.sourceforge.net
+ 
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+ 
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111, USA.
+ 
+*****************************************************************/
+
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+#include <avdec_private.h>
+
+
+int probe_ref(bgav_input_context_t * input)
+  {
+  uint8_t probe_data[11];
+  if(bgav_input_get_data(input, probe_data, 11) < 11)
+    return 0;
+
+  if(!strncasecmp(probe_data, "[Reference]", 11))
+    return 1;
+  return 0;
+  }
+
+int parse_ref(bgav_redirector_context_t * r)
+  {
+  char * buffer = (char *)0;
+  int buffer_alloc = 0;
+  char * pos;
+    
+  if(!bgav_input_read_line(r->input, &buffer, &buffer_alloc))
+    return 0;
+
+  if(strncasecmp(buffer, "[Reference]", 11))
+    return 0;
+  
+  while(1)
+    {
+    if(!bgav_input_read_line(r->input, &buffer, &buffer_alloc))
+      break;
+
+    if(!strncasecmp(buffer, "ref", 3) && isdigit(buffer[3]))
+      {
+      pos = strchr(buffer, '=');
+      if(pos)
+        {
+        pos++;
+        r->num_urls++;
+        r->urls = realloc(r->urls, r->num_urls * sizeof(*(r->urls)));
+        memset(r->urls + r->num_urls - 1, 0, sizeof(*(r->urls)));
+        
+        r->urls[r->num_urls-1].name = bgav_sprintf("Stream %d (%s)",
+                                                   r->num_urls,
+                                                   pos);
+        r->urls[r->num_urls-1].url = bgav_strndup(pos, NULL);
+        }
+      }
+    }
+  return 1;
+  }
+
+bgav_redirector_t bgav_redirector_ref = 
+  {
+    probe: probe_ref,
+    parse: parse_ref
+  };
