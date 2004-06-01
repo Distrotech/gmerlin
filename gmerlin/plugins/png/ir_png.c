2a2325740c79bc8766034b56d7a6ddc19ff2dac2
gmerlin <gmerlin@214d8ac5-8513-4f59-80f8-2f39b674a24a>
2004-06-01 18:39:56 +0000

    * Added png plugins
    git-svn-id: svn://svn.code.sf.net/p/gmerlin/code/trunk@115 214d8ac5-8513-4f59-80f8-2f39b674a24a

---
+++ gmerlin/plugins/png/ir_png.c
@@ -0,0 +1,197 @@
+/*****************************************************************
+ 
+  ir_png.c
+ 
+  Copyright (c) 2003-2004 by Burkhard Plaum - plaum@ipf.uni-stuttgart.de
+ 
+  http://gmerlin.sourceforge.net
+ 
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+ 
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111, USA.
+ 
+*****************************************************************/
+
+#include <stdlib.h>
+#include <stdio.h>
+
+#include <plugin.h>
+
+#include <png.h>
+
+typedef struct
+  {
+  png_structp png_ptr;
+  png_infop info_ptr;
+  png_infop end_info;
+
+  gavl_video_format_t format;
+
+  FILE * file;
+  } png_t;
+
+void * create_png()
+  {
+  png_t * ret;
+  ret = calloc(1, sizeof(*ret));
+
+  return ret;
+  }
+
+void destroy_png(void* priv)
+  {
+  png_t * png = (png_t*)priv;
+
+  free(png);
+  }
+
+int read_header_png(void * priv, const char * filename,
+                     gavl_video_format_t * format)
+  {
+  int bit_depth;
+  int color_type;
+  int has_alpha = 0;
+
+  png_byte signature[8];
+  png_t * png = (png_t*)priv;
+    
+  png->file = fopen(filename, "rb");
+
+  if(!png->file)
+    goto fail;
+
+  if(fread(signature, 1, 8, png->file) < 8)
+    goto fail;
+
+  if(png_sig_cmp(signature, 0, 8))
+    goto fail;
+
+  png->png_ptr = png_create_read_struct
+    (PNG_LIBPNG_VER_STRING, (png_voidp)0,
+     NULL, NULL);
+
+  setjmp(png_jmpbuf(png->png_ptr));
+
+  png->info_ptr = png_create_info_struct(png->png_ptr);
+
+  if(!png->info_ptr)
+    goto fail;
+                                                                                
+  png->end_info = png_create_info_struct(png->png_ptr);
+  if(!png->end_info)
+    return 0;
+
+  png_init_io(png->png_ptr, png->file);
+  png_set_sig_bytes(png->png_ptr, 8);
+                                                                                
+  png_read_info(png->png_ptr, png->info_ptr);
+
+  format->frame_width  = png_get_image_width(png->png_ptr, png->info_ptr);
+  format->frame_height = png_get_image_height(png->png_ptr, png->info_ptr);
+
+  format->image_width  = format->frame_width;
+  format->image_height = format->frame_height;
+  format->pixel_width = 1;
+  format->pixel_height = 1;
+
+  bit_depth  = png_get_bit_depth(png->png_ptr,  png->info_ptr);
+  color_type = png_get_color_type(png->png_ptr, png->info_ptr);
+
+  switch(color_type)
+    {
+    case PNG_COLOR_TYPE_GRAY:       /*  (bit depths 1, 2, 4, 8, 16) */
+      if(bit_depth < 8)
+        png_set_gray_1_2_4_to_8(png->png_ptr);
+      if (png_get_valid(png->png_ptr, png->info_ptr, PNG_INFO_tRNS))
+        {
+        png_set_tRNS_to_alpha(png->png_ptr);
+        has_alpha = 1;
+        }
+      png_set_gray_to_rgb(png->png_ptr);
+      break;
+    case PNG_COLOR_TYPE_GRAY_ALPHA: /*  (bit depths 8, 16) */
+      if(bit_depth == 16)
+        png_set_strip_16(png->png_ptr);
+      png_set_gray_to_rgb(png->png_ptr);
+      break;
+    case PNG_COLOR_TYPE_PALETTE:    /*  (bit depths 1, 2, 4, 8) */
+      png_set_palette_to_rgb(png->png_ptr);
+      if (png_get_valid(png->png_ptr, png->info_ptr, PNG_INFO_tRNS))
+        {
+        png_set_tRNS_to_alpha(png->png_ptr);
+        has_alpha = 1;
+        }
+      break;
+    case PNG_COLOR_TYPE_RGB:        /*  (bit_depths 8, 16) */
+      if(png_get_valid(png->png_ptr, png->info_ptr, PNG_INFO_tRNS))
+        {
+        png_set_tRNS_to_alpha(png->png_ptr);
+        has_alpha = 1;
+        }
+      if(bit_depth == 16)
+        png_set_strip_16(png->png_ptr);
+      break;
+    case PNG_COLOR_TYPE_RGB_ALPHA:  /*  (bit_depths 8, 16) */
+      if(bit_depth == 16)
+        png_set_strip_16(png->png_ptr);
+      break;
+    }
+
+  if(has_alpha)
+    format->colorspace = GAVL_RGBA_32;
+  else
+    format->colorspace = GAVL_RGB_24;
+    
+  gavl_video_format_copy(&(png->format), format);
+  return 1;
+  fail:
+  return 0;
+
+  }
+
+int read_image_png(void * priv, gavl_video_frame_t * frame)
+  {
+  int i;
+  unsigned char ** rows;
+  png_t * png = (png_t*)priv;
+
+  setjmp(png_jmpbuf(png->png_ptr));
+  
+  rows = malloc(png->format.frame_height * sizeof(*rows));
+  for(i = 0; i < png->format.frame_height; i++)
+    rows[i] = frame->planes[0] + i * frame->strides[0];
+
+  png_read_image(png->png_ptr, rows);
+  png_read_end(png->png_ptr, png->end_info);
+
+  png_destroy_read_struct(&(png->png_ptr), &(png->info_ptr),
+                          &(png->end_info));
+  fclose(png->file);
+  free(rows);
+  
+  return 1;
+  }
+
+bg_image_reader_plugin_t the_plugin =
+  {
+    common:
+    {
+      name:          "ir_png",
+      long_name:     "PNG loader",
+      mimetypes:     (char*)0,
+      extensions:    "png",
+      type:          BG_PLUGIN_IMAGE_READER,
+      flags:         BG_PLUGIN_FILE,
+      create:        create_png,
+      destroy:       destroy_png,
+    },
+    read_header: read_header_png,
+    read_image:  read_image_png,
+  };
+
+
---
+++ gmerlin/plugins/png/iw_png.c
@@ -0,0 +1,172 @@
+/*****************************************************************
+ 
+  iw_png.c
+ 
+  Copyright (c) 2003-2004 by Burkhard Plaum - plaum@ipf.uni-stuttgart.de
+ 
+  http://gmerlin.sourceforge.net
+ 
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+ 
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111, USA.
+ 
+*****************************************************************/
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#include <plugin.h>
+#include <utils.h>
+
+#include <png.h>
+
+#define PADD(i, size) i = ((i + size - 1) / size) * size
+
+/* PNG writer */
+
+typedef struct
+  {
+  FILE * output;
+  gavl_video_format_t format;
+
+  png_structp png_ptr;
+  png_infop   info_ptr;
+  
+  int compression_level;
+  } png_t;
+
+void * create_png()
+  {
+  png_t * ret;
+  ret = calloc(1, sizeof(*ret));
+
+  return ret;
+  }
+
+void destroy_png(void * priv)
+  {
+  png_t * png = (png_t*)priv;
+  free(png);
+  }
+
+int write_header_png(void * priv, const char * filename_base,
+                     gavl_video_format_t * format)
+  {
+  char * filename;
+  int color_type;
+  
+  png_t * png = (png_t*)priv;
+  
+  filename = bg_sprintf("%s.png", filename_base);
+  png->output = fopen(filename, "wb");
+  free(filename);
+  if(!png->output)
+    return 0;
+
+  png->png_ptr = png_create_write_struct(PNG_LIBPNG_VER_STRING, NULL,
+                                         NULL, NULL);
+
+  png->info_ptr = png_create_info_struct(png->png_ptr);
+  setjmp(png_jmpbuf(png->png_ptr));
+  png_init_io(png->png_ptr, png->output);
+
+  if(gavl_colorspace_has_alpha(format->colorspace))
+    {
+    color_type = PNG_COLOR_TYPE_RGBA;
+    format->colorspace = GAVL_RGBA_32;
+    }
+  else
+    {
+    color_type = PNG_COLOR_TYPE_RGB;
+    format->colorspace = GAVL_RGB_24;
+    }
+
+  png_set_compression_level(png->png_ptr, png->compression_level);
+  png_set_IHDR(png->png_ptr, png->info_ptr,
+               format->image_width,
+               format->image_height,
+               8, color_type, PNG_INTERLACE_NONE,
+               PNG_COMPRESSION_TYPE_DEFAULT, PNG_FILTER_TYPE_DEFAULT);
+  
+  gavl_video_format_copy(&(png->format), format);
+  
+  return 1;
+  }
+
+int write_image_png(void * priv, gavl_video_frame_t * frame)
+  {
+  int i;
+  unsigned char ** rows;
+  png_t * png = (png_t*)priv;
+
+  rows = malloc(png->format.image_height * sizeof(*rows));
+
+  for(i = 0; i < png->format.image_height; i++)
+    rows[i] = frame->planes[0] + i * frame->strides[0];
+
+  png_set_rows(png->png_ptr, png->info_ptr, rows);
+  png_write_png(png->png_ptr, png->info_ptr, PNG_TRANSFORM_IDENTITY, NULL);
+ 
+  png_destroy_write_struct(&png->png_ptr, &png->info_ptr);
+  fclose(png->output);
+  free(rows);
+  return 1;
+  }
+
+/* Configuration stuff */
+
+static bg_parameter_info_t parameters[] =
+  {
+    {
+      name:        "compression",
+      long_name:   "Compression level",
+      type:        BG_PARAMETER_SLIDER_INT,
+      val_min:     { val_i: 0 },
+      val_max:     { val_i: 9 },
+      val_default: { val_i: 9 },
+    },
+    { /* End of parameters */ }
+  };
+
+static bg_parameter_info_t * get_parameters_png(void * p)
+  {
+  return parameters;
+  }
+
+static void set_parameter_png(void * p, char * name,
+                               bg_parameter_value_t * val)
+  {
+  png_t * png;
+  png = (png_t *)p;
+  
+  if(!name)
+    return;
+
+  if(!strcmp(name, "compression"))
+    png->compression_level = val->val_i;
+  }
+
+bg_image_writer_plugin_t the_plugin =
+  {
+    common:
+    {
+      name:           "iw_png",
+      long_name:      "PNG writer",
+      mimetypes:      (char*)0,
+      extensions:     "png",
+      type:           BG_PLUGIN_IMAGE_WRITER,
+      flags:          BG_PLUGIN_FILE,
+      create:         create_png,
+      destroy:        destroy_png,
+      get_parameters: get_parameters_png,
+      set_parameter:  set_parameter_png
+    },
+    write_header: write_header_png,
+    write_image:  write_image_png,
+  };
