0e54d633acfb437d561987941f6f67d9d4206398
gmerlin <gmerlin@214d8ac5-8513-4f59-80f8-2f39b674a24a>
2005-10-13 23:04:28 +0000

    * Update
    git-svn-id: svn://svn.code.sf.net/p/gmerlin/code/trunk@619 214d8ac5-8513-4f59-80f8-2f39b674a24a

--- gmerlin/lib/player_ov.c
+++ gmerlin/lib/player_ov.c
@@ -353,7 +353,7 @@ void * bg_player_ov_thread(void * data)
     bg_player_time_get(ctx->player, 1, &current_time);
 #if 0
     fprintf(stderr, "F: %f, C: %f\n",
-            gavl_time_to_seconds(frame->time),
+            gavl_time_to_seconds(ctx->frame->time_scaled),
             gavl_time_to_seconds(current_time));
 #endif
 
