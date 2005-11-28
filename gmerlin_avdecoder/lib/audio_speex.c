2a305ad213ff4983329c337fffcb4491dc8a0f71
one78 <one78@214d8ac5-8513-4f59-80f8-2f39b674a24a>
2005-11-28 14:47:41 +0000

    * Speex compilation fix
    git-svn-id: svn://svn.code.sf.net/p/gmerlin/code/trunk@640 214d8ac5-8513-4f59-80f8-2f39b674a24a

--- gmerlin_avdecoder/lib/audio_speex.c
+++ gmerlin_avdecoder/lib/audio_speex.c
@@ -22,10 +22,10 @@
 #include <string.h>
 
 #include <avdec_private.h>
-#include <speex.h>
-#include <speex_header.h>
-#include <speex_stereo.h>
-#include <speex_callbacks.h>
+#include <speex/speex.h>
+#include <speex/speex_header.h>
+#include <speex/speex_stereo.h>
+#include <speex/speex_callbacks.h>
 
 typedef struct
   {
