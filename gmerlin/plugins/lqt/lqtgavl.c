c38525272cf8e4858562c39e68fccbde61fa51cf
gmerlin <gmerlin@214d8ac5-8513-4f59-80f8-2f39b674a24a>
2005-12-03 15:42:09 +0000

    * lqt -> gavl wrapper
    git-svn-id: svn://svn.code.sf.net/p/gmerlin/code/trunk@642 214d8ac5-8513-4f59-80f8-2f39b674a24a

---
+++ gmerlin/plugins/lqt/lqtgavl.c
@@ -0,0 +1,397 @@
+/*****************************************************************
+
+  lqtgavl.c
+
+  Copyright (c) 2005 by Burkhard Plaum - plaum@ipf.uni-stuttgart.de
+
+  http://gmerlin.sourceforge.net
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111, USA.
+
+*****************************************************************/
+#include <stdlib.h>
+#include <lqtgavl.h>
+#include <colormodels.h>
+
+/* Sampleformat conversion */
+
+static struct
+  {
+  lqt_sample_format_t lqt;
+  gavl_sample_format_t gavl;
+  }
+sampleformats[] =
+  {
+    { LQT_SAMPLE_INT8,  GAVL_SAMPLE_S8 },
+    { LQT_SAMPLE_UINT8, GAVL_SAMPLE_U8 },
+    { LQT_SAMPLE_INT16, GAVL_SAMPLE_S16 },
+    { LQT_SAMPLE_INT32, GAVL_SAMPLE_S32 },
+    { LQT_SAMPLE_FLOAT, GAVL_SAMPLE_FLOAT }
+  };
+
+static gavl_sample_format_t
+sampleformat_lqt_2_gavl(lqt_sample_format_t format)
+  {
+  int i;
+  for(i = 0; i < sizeof(sampleformats) / sizeof(sampleformats[0]); i++)
+    {
+    if(sampleformats[i].lqt == format)
+      return sampleformats[i].gavl;
+    }
+  return GAVL_SAMPLE_NONE;
+  }
+
+/* Chroma placement */
+
+static struct
+  {
+  lqt_chroma_placement_t lqt;
+  gavl_chroma_placement_t gavl;
+  }
+chroma_placements[] =
+  {
+    {  LQT_CHROMA_PLACEMENT_DEFAULT, GAVL_CHROMA_PLACEMENT_DEFAULT },
+    {  LQT_CHROMA_PLACEMENT_MPEG2,   GAVL_CHROMA_PLACEMENT_MPEG2   },
+    {  LQT_CHROMA_PLACEMENT_DVPAL,   GAVL_CHROMA_PLACEMENT_DVPAL   },
+  };
+
+static gavl_chroma_placement_t
+chroma_placement_lqt_2_gavl(lqt_chroma_placement_t chroma_placement)
+  {
+  int i;
+  for(i = 0; i < sizeof(chroma_placements) / sizeof(chroma_placements[0]); i++)
+    {
+    if(chroma_placements[i].lqt == chroma_placement)
+      return chroma_placements[i].gavl;
+    }
+  return GAVL_CHROMA_PLACEMENT_DEFAULT;
+  }
+
+/* Interlace mode */
+
+static struct
+  {
+  lqt_interlace_mode_t lqt;
+  gavl_interlace_mode_t gavl;
+  }
+interlace_modes[] =
+  {
+    { LQT_INTERLACE_NONE,         GAVL_INTERLACE_NONE},
+    { LQT_INTERLACE_TOP_FIRST,    GAVL_INTERLACE_TOP_FIRST },
+    { LQT_INTERLACE_BOTTOM_FIRST, GAVL_INTERLACE_BOTTOM_FIRST },
+  };
+
+static gavl_interlace_mode_t
+interlace_mode_lqt_2_gavl(lqt_interlace_mode_t interlace_mode)
+  {
+  int i;
+  for(i = 0; i < sizeof(interlace_modes) / sizeof(interlace_modes[0]); i++)
+    {
+    if(interlace_modes[i].lqt == interlace_mode)
+      return interlace_modes[i].gavl;
+    }
+  return GAVL_INTERLACE_NONE;
+  }
+
+/* Pixelformats */
+
+static struct
+  {
+  int lqt;
+  gavl_pixelformat_t gavl;
+  }
+pixelformats[] =
+  {
+    { BC_RGB565,       GAVL_RGB_16 },
+    { BC_BGR565,       GAVL_BGR_16 },
+    { BC_BGR888,       GAVL_BGR_24 },
+    { BC_BGR8888,      GAVL_BGR_32 },
+    { BC_RGB888,       GAVL_RGB_24 },
+    { BC_RGBA8888,     GAVL_RGBA_32 },
+    { BC_RGB161616,    GAVL_RGB_48 },
+    { BC_RGBA16161616, GAVL_RGBA_64 },
+    { BC_YUVA8888,     GAVL_YUVA_32 },
+    { BC_YUV422,       GAVL_YUY2 },
+    { BC_YUV420P,      GAVL_YUV_420_P },
+    { BC_YUV422P,      GAVL_YUV_422_P },
+    { BC_YUV444P,      GAVL_YUV_444_P },
+    { BC_YUVJ420P,     GAVL_YUVJ_420_P },
+    { BC_YUVJ422P,     GAVL_YUVJ_422_P },
+    { BC_YUVJ444P,     GAVL_YUVJ_444_P },
+    { BC_YUV411P,      GAVL_YUV_411_P },
+    { BC_YUV422P16,    GAVL_YUV_422_P_16 },
+    { BC_YUV444P16,    GAVL_YUV_444_P_16 },
+  };
+
+static gavl_pixelformat_t
+pixelformat_lqt_2_gavl(int pixelformat)
+  {
+  int i;
+  for(i = 0; i < sizeof(pixelformats) / sizeof(pixelformats[0]); i++)
+    {
+    if(pixelformats[i].lqt == pixelformat)
+      return pixelformats[i].gavl;
+    }
+  return GAVL_PIXELFORMAT_NONE;
+  }
+
+  
+void lqt_gavl_add_audio_track(quicktime_t * file,
+                               gavl_audio_format_t * format,
+                               lqt_codec_info_t * codec)
+  {
+  int track = quicktime_audio_tracks(file);
+  
+  lqt_add_audio_track(file, format->num_channels, format->samplerate,
+                      16, codec);
+
+  format->sample_format =
+    sampleformat_lqt_2_gavl(lqt_get_sample_format(file, track));
+  format->interleave_mode = GAVL_INTERLEAVE_ALL;
+  }
+
+void lqt_gavl_add_video_track(quicktime_t * file,
+                               gavl_video_format_t * format,
+                               lqt_codec_info_t * codec)
+  {
+  int track = quicktime_video_tracks(file);
+  //  fprintf(stderr, "lqt_gavl_add_video_track: track: %d\n", track);
+
+  lqt_add_video_track(file, format->image_width, format->image_height,
+                      format->frame_duration, format->timescale,
+                      codec);
+  lqt_set_pixel_aspect(file, track, format->pixel_width, format->pixel_height);
+
+  //  fprintf(stderr, "lqt_gavl_add_video_track: %d %d %s\n", track, lqt_get_cmodel(file, track),
+  // lqt_colormodel_to_string(lqt_get_cmodel(file, track)));
+  
+  format->pixelformat = pixelformat_lqt_2_gavl(lqt_get_cmodel(file, track));
+  }
+
+
+
+void lqt_gavl_encode_video(quicktime_t * file, int track,
+                           gavl_video_frame_t * frame, uint8_t ** rows)
+  {
+  int i, height;
+
+  if(lqt_colormodel_is_planar(lqt_get_cmodel(file, track)))
+    {
+    lqt_set_row_span(file, track, frame->strides[0]);
+    lqt_set_row_span_uv(file, track, frame->strides[1]);
+    lqt_encode_video(file, frame->planes, track, frame->time_scaled);
+    }
+  else
+    {
+    height = quicktime_video_height(file, track);
+    for(i = 0; i < height; i++)
+      {
+      lqt_set_row_span(file, track, frame->strides[0]);
+      rows[i] = frame->planes[0] + i * frame->strides[0];
+      }
+    lqt_encode_video(file, rows, track, frame->time_scaled);
+    }
+  }
+
+void lqt_gavl_encode_audio(quicktime_t * file, int track,
+                           gavl_audio_frame_t * frame)
+  {
+  lqt_encode_audio_raw(file, frame->samples.s_8, frame->valid_samples,
+                       track);
+  }
+
+/* Decoding */
+
+int lqt_gavl_get_audio_format(quicktime_t * file,
+                              int track,
+                              gavl_audio_format_t * format)
+  {
+  if(track >= quicktime_audio_tracks(file) ||
+     track < 0)
+    return 0;
+  
+  format->num_channels = quicktime_track_channels(file, track);
+  format->samplerate = quicktime_sample_rate(file, track);
+  format->sample_format =
+    sampleformat_lqt_2_gavl(lqt_get_sample_format(file, track));
+  format->interleave_mode = GAVL_INTERLEAVE_ALL;
+  gavl_set_channel_setup(format);
+  return 1;
+  }
+
+int lqt_gavl_get_video_format(quicktime_t * file,
+                              int track,
+                              gavl_video_format_t * format)
+  {
+  int constant_framerate;
+  if(track >= quicktime_video_tracks(file) ||
+     track < 0)
+    return 0;
+
+  format->image_width = quicktime_video_width(file, track);
+  format->image_height = quicktime_video_height(file, track);
+
+  format->frame_width = format->image_width;
+  format->frame_height = format->image_height;
+
+  lqt_get_pixel_aspect(file, track, &format->pixel_width,
+                       &format->pixel_height);
+
+  format->timescale = lqt_video_time_scale(file, track);
+  format->frame_duration = lqt_frame_duration(file, track,
+                                              &constant_framerate);
+
+  if(!constant_framerate)
+    format->framerate_mode = GAVL_FRAMERATE_VARIABLE;
+  else
+    format->framerate_mode = GAVL_FRAMERATE_CONSTANT;
+  
+  format->chroma_placement =
+    chroma_placement_lqt_2_gavl(lqt_get_chroma_placement(file, track));
+  
+  format->interlace_mode =
+    interlace_mode_lqt_2_gavl(lqt_get_interlace_mode(file, track));
+
+  format->pixelformat = pixelformat_lqt_2_gavl(lqt_get_cmodel(file, track));
+  
+  return 1;
+  }
+
+int lqt_gavl_decode_video(quicktime_t * file, int track,
+                          gavl_video_frame_t * frame, uint8_t ** rows)
+  {
+  int i, height;
+  if(quicktime_video_position(file, track) >=
+     quicktime_video_length(file, track))
+    return 0;
+  
+  frame->time_scaled = lqt_frame_time(file, track);
+
+
+
+  if(lqt_colormodel_is_planar(lqt_get_cmodel(file, track)))
+    {
+    lqt_set_row_span(file, track, frame->strides[0]);
+    lqt_set_row_span_uv(file, track, frame->strides[1]);
+    lqt_decode_video(file, frame->planes, track);
+    }
+  else
+    {
+    height = quicktime_video_height(file, track);
+    for(i = 0; i < height; i++)
+      {
+      lqt_set_row_span(file, track, frame->strides[0]);
+      rows[i] = frame->planes[0] + i * frame->strides[0];
+      }
+    lqt_decode_video(file, rows, track);
+    }
+  return 1;
+  }
+
+int lqt_gavl_decode_audio(quicktime_t * file, int track,
+                          gavl_audio_frame_t * frame,
+                          int samples)
+  {
+  int64_t pos;
+
+  pos = quicktime_audio_position(file, track);
+  lqt_decode_audio_raw(file, frame->samples.s_8, samples, track);
+  frame->valid_samples = lqt_last_audio_position(file, track) - pos;
+  //  fprintf(stderr, "lqt_decode_audio_raw: %d\n", frame->valid_samples);
+  return frame->valid_samples;
+  }
+
+void lqt_gavl_seek(quicktime_t * file, gavl_time_t * time)
+  {
+  int imax;
+  int i;
+  int64_t video_time_save  = -1;
+  int video_timescale_save = -1;
+
+  int64_t time_scaled;
+  int timescale;
+
+  /* We synchronize to the first video track */
+
+  imax = quicktime_video_tracks(file);
+
+  for(i = 0; i < imax; i++)
+    {
+    timescale = lqt_video_time_scale(file, i);
+    time_scaled = gavl_time_scale(timescale, *time);
+    lqt_seek_video(file, i, time_scaled);
+    if(!i)
+      {
+      video_timescale_save = timescale;
+      video_time_save      = time_scaled;
+      }
+    }
+
+  if(video_time_save >= 0)
+    *time = gavl_time_unscale(video_timescale_save, video_time_save);
+
+  imax = quicktime_audio_tracks(file);
+  for(i = 0; i < imax; i++)
+    {
+    timescale = quicktime_sample_rate(file, i);
+    time_scaled = gavl_time_scale(timescale, *time);
+    quicktime_set_audio_position(file, time_scaled, i);
+    }
+  
+  }
+
+uint8_t ** lqt_gavl_rows_create(quicktime_t * file, int track)
+  {
+  uint8_t ** ret;
+
+  if(lqt_colormodel_is_planar(lqt_get_cmodel(file, track)))
+    return (uint8_t**)0;
+  
+  ret = malloc(sizeof(*ret) * quicktime_video_height(file, track));
+  return ret;
+  }
+
+
+void lqt_gavl_rows_destroy(uint8_t** rows)
+  {
+  if(rows)
+    free(rows);
+  }
+
+gavl_time_t lqt_gavl_duration(quicktime_t * file)
+  {
+  gavl_time_t ret = 0, time;
+  int i, imax;
+  int timescale;
+  int64_t time_scaled;
+  
+  imax = quicktime_audio_tracks(file);
+  for(i = 0; i < imax; i++)
+    {
+    timescale = quicktime_sample_rate(file, i);
+    time_scaled = quicktime_audio_length(file, i);
+
+    time = gavl_time_unscale(timescale, time_scaled);
+    if(ret < time)
+      ret = time;
+    }
+
+  imax = quicktime_video_tracks(file);
+  for(i = 0; i < imax; i++)
+    {
+    timescale = lqt_video_time_scale(file, i);
+    time_scaled = lqt_video_duration(file, i);
+    
+    time = gavl_time_unscale(timescale, time_scaled);
+    if(ret < time)
+      ret = time;
+    }
+  return ret;
+  }
