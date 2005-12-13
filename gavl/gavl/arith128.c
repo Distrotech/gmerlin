77cb2ace29df1129f9d648eb4aae169b5c72dce1
gmerlin <gmerlin@214d8ac5-8513-4f59-80f8-2f39b674a24a>
2005-12-13 00:18:06 +0000

    * Forgot these files
    git-svn-id: svn://svn.code.sf.net/p/gmerlin/code/trunk@655 214d8ac5-8513-4f59-80f8-2f39b674a24a

---
+++ gavl/gavl/arith128.c
@@ -0,0 +1,158 @@
+/*****************************************************************
+ 
+  arith128.c
+ 
+  Copyright (c) 2005 by Burkhard Plaum - plaum@ipf.uni-stuttgart.de
+ 
+  http://gmerlin.sourceforge.net
+ 
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+ 
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111, USA.
+ 
+*****************************************************************/
+
+/* 128 bit arithmetic routines needed for overflow save
+ * 64 bit rescaling functions.
+ *
+ * Mostly copied and pasted from the qofmath128.[ch] files by
+ * Copyright (C) 2004 Linas Vepstas <linas@linas.org>,
+ * which are part of gnucash (http://gnucash.org)
+ */
+
+#include <inttypes.h>
+#include <arith128.h>
+
+#define HIBIT (0x8000000000000000ULL)
+
+/** Multiply a pair of signed 64-bit numbers,
+ *  returning a signed 128-bit number.
+ */
+void gavl_int128_mult(int64_t a, int64_t b, gavl_int128_t * ret)
+  {
+  uint64_t a0, a1;
+  uint64_t b0, b1;
+  uint64_t d, d0, d1;
+  uint64_t e, e0, e1;
+  uint64_t f, f0, f1;
+  uint64_t g, g0, g1;
+  uint64_t sum, carry, roll, pmax;
+
+  ret->isneg = 0;
+  if (0>a)
+  {
+    ret->isneg = !ret->isneg;
+    a = -a;
+  }
+
+  if (0>b)
+  {
+    ret->isneg = !ret->isneg;
+    b = -b;
+  }
+
+  a1 = a >> 32;
+  a0 = a - (a1<<32);
+
+  b1 = b >> 32;
+  b0 = b - (b1<<32);
+
+  d = a0*b0;
+  d1 = d >> 32;
+  d0 = d - (d1<<32);
+
+  e = a0*b1;
+  e1 = e >> 32;
+  e0 = e - (e1<<32);
+
+  f = a1*b0;
+  f1 = f >> 32;
+  f0 = f - (f1<<32);
+
+  g = a1*b1;
+  g1 = g >> 32;
+  g0 = g - (g1<<32);
+
+  sum = d1+e0+f0;
+  carry = 0;
+  /* Can't say 1<<32 cause cpp will goof it up; 1ULL<<32 might work */
+  roll = 1<<30;
+  roll <<= 2;
+  pmax = roll-1;
+  while (pmax < sum)
+  {
+    sum -= roll;
+    carry ++;
+  }
+
+  ret->lo = d0 + (sum<<32);
+  ret->hi = carry + e1 + f1 + g0 + (g1<<32);
+
+  ret->isbig = ret->hi || (ret->lo >> 63);
+  }
+
+static void shiftleft128(gavl_int128_t * x)
+  {
+  uint64_t sbit;
+  sbit = x->lo & HIBIT;
+  x->hi <<= 1;
+  x->lo <<= 1;
+  x->isbig = 0;
+  if (sbit)
+  {
+    x->hi |= 1;
+    x->isbig = 1;
+    return;
+  }
+  if (x->hi)
+  {
+    x->isbig = 1;
+  }
+}
+
+
+/** Divide a signed 128-bit number by a signed 64-bit,
+ *  returning a signed 128-bit number.
+ */
+
+void gavl_int128_div(gavl_int128_t * n, int64_t d, gavl_int128_t * ret)
+  {
+  int i;
+  uint64_t remainder = 0;
+
+  gavl_int128_copy(ret, n);
+  
+  if (0 > d)
+  {
+    d = -d;
+    ret->isneg = !ret->isneg;
+  }
+
+  /* Use grade-school long division algorithm */
+  for (i=0; i<128; i++)
+  {
+    uint64_t sbit = HIBIT & ret->hi;
+    remainder <<= 1;
+    if (sbit) remainder |= 1;
+    shiftleft128(ret);
+    if (remainder >= d)
+    {
+       remainder -= d;
+       ret->lo |= 1;
+    }
+  }
+
+  /* compute the carry situation */
+  ret->isbig = (ret->hi || (ret->lo >> 63));
+  
+  }
+
+void gavl_int128_copy(gavl_int128_t *dst, gavl_int128_t * src)
+  {
+  memcpy(dst, src, sizeof(*dst));
+  }
---
+++ gavl/include/arith128.h
@@ -0,0 +1,48 @@
+/*****************************************************************
+ 
+  arith128.h
+ 
+  Copyright (c) 2005 by Burkhard Plaum - plaum@ipf.uni-stuttgart.de
+ 
+  http://gmerlin.sourceforge.net
+ 
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+ 
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111, USA.
+ 
+*****************************************************************/
+
+/* 128 bit arithmetic routines needed for overflow save
+ * 64 bit rescaling functions.
+ *
+ * Mostly copied and pasted from the qofmath128.[ch] files by
+ * Copyright (C) 2004 Linas Vepstas <linas@linas.org>,
+ * which are part of gnucash (http://gnucash.org)
+ */
+
+typedef struct {
+  uint64_t hi;
+  uint64_t lo;
+  short isneg;    /**< sign-bit -- T if number is negative */
+  short isbig;    /**< sizeflag -- T if number won't fit in signed 64-bit */
+} gavl_int128_t;
+
+
+/** Multiply a pair of signed 64-bit numbers,
+ *  returning a signed 128-bit number.
+ */
+void gavl_int128_mult(int64_t a, int64_t b, gavl_int128_t * ret);
+
+/** Divide a signed 128-bit number by a signed 64-bit,
+ *  returning a signed 128-bit number.
+ */
+
+void gavl_int128_div(gavl_int128_t * n, int64_t d, gavl_int128_t * ret);
+
+
+void gavl_int128_copy(gavl_int128_t *dst, gavl_int128_t * src);
