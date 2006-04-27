3848f68ec17c63cec2964aa2707d3f3574019d64
one78 <one78@214d8ac5-8513-4f59-80f8-2f39b674a24a>
2006-04-27 12:59:02 +0000

    * cddb skeletton
    git-svn-id: svn://svn.code.sf.net/p/gmerlin/code/trunk@845 214d8ac5-8513-4f59-80f8-2f39b674a24a

---
+++ gmerlin/plugins/cdaudio/cddb.c
@@ -0,0 +1,19 @@
+
+
+#include <string.h>
+#include <stdlib.h>
+
+#include <utils.h>
+#include "cdaudio.h"
+
+int bg_cdaudio_get_metadata_cddb(bg_cdaudio_index_t * idx,
+                                 bg_track_info_t * info,
+                                 char * cddb_host,
+                                 int cddb_port,
+                                 char * cddb_proxy_host,
+                                 int cddb_proxy_port,
+                                 char * cddb_proxy_user,
+                                 char * cddb_proxy_pass)
+  {
+  return 0;
+  }
