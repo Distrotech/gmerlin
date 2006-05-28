4b0b255d77f3478b559b7a851b7015b21824120b
gmerlin <gmerlin@214d8ac5-8513-4f59-80f8-2f39b674a24a>
2006-05-28 23:54:47 +0000

    * spumux encoder
    git-svn-id: svn://svn.code.sf.net/p/gmerlin/code/trunk@891 214d8ac5-8513-4f59-80f8-2f39b674a24a

---
+++ gmerlin/plugins/png/e_spumux.c
@@ -0,0 +1,248 @@
+/*****************************************************************
+ 
+  e_spumux.c
+ 
+  Copyright (c) 2006 by Burkhard Plaum - plaum@ipf.uni-stuttgart.de
+ 
+  http://gmerlin.sourceforge.net
+ 
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+ 
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111, USA.
+ 
+*****************************************************************/
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#include <plugin.h>
+#include <utils.h>
+
+#include <png.h>
+
+#include "pngwriter.h"
+
+typedef struct
+  {
+  bg_pngwriter_t w; /* Must be first in the struct */
+  FILE * xml_file;
+
+  gavl_video_format_t format;
+  int subtitles_read;
+  gavl_video_frame_t * subframe;
+  
+  char * filename_template;
+  char * filename;
+  int subtitles_written;
+  } spumux_t;
+
+static void * create_spumux()
+  {
+  spumux_t * ret;
+  ret = calloc(1, sizeof(*ret));
+  ret->w.bit_mode = BITS_8;
+  ret->subframe = gavl_video_frame_create(NULL);
+  return ret;
+  }
+
+static char * extension = "xml";
+
+static const char * get_extension_spumux(void * priv)
+  {
+  return extension;
+  }
+
+static int open_spumux(void * priv, const char * filename, bg_metadata_t * metadata)
+  {
+  char * pos;
+  spumux_t * spumux = (spumux_t*)priv;
+  
+  spumux->filename = bg_strdup(spumux->filename, filename);
+  spumux->filename_template = bg_strdup(spumux->filename_template, filename);
+
+  pos = strrchr(spumux->filename_template, '.');
+  if(pos) *pos = '\0';
+
+  spumux->filename_template = bg_strcat(spumux->filename_template, "_%05d.png");
+  
+  spumux->xml_file = fopen(filename, "w");
+
+  fprintf(spumux->xml_file, "<subpictures>\n  <stream>\n");
+
+  return 1;
+  }
+
+static int add_subtitle_overlay_stream_spumux(void * priv, const char * language,
+                                              gavl_video_format_t * format)
+  {
+  spumux_t * spumux = (spumux_t*)priv;
+  gavl_video_format_copy((&spumux->format), format);
+  spumux->format.pixelformat = GAVL_RGBA_32;
+  return 1;
+  }
+
+static int start_spumux(void * priv)
+  {
+  return 1;
+  }
+
+static void get_subtitle_overlay_format_spumux(void * priv, int stream,
+                                               gavl_video_format_t*ret)
+  {
+  spumux_t * spumux = (spumux_t*)priv;
+  gavl_video_format_copy(ret, (&spumux->format));
+  }
+
+static void print_time(FILE * out, gavl_time_t time, gavl_video_format_t * format)
+  {
+  int h, m, s, f;
+
+  f = gavl_time_to_frames(format->timescale, format->frame_duration, time % GAVL_TIME_SCALE);
+  time /= GAVL_TIME_SCALE;
+  s = time % 60;
+  time /= 60;
+  m = time % 60;
+  time /= 60;
+  h = time % 60;
+  fprintf(out, "%02d:%02d:%02d.%02d", h, m, s, f);
+  }
+
+static void write_subtitle_overlay_spumux(void * priv, gavl_overlay_t * ovl, int stream)
+  {
+  char * image_filename;
+  spumux_t * spumux = (spumux_t*)priv;
+  gavl_video_format_t tmp_format;
+  gavl_video_format_copy(&tmp_format, (&spumux->format));
+
+  tmp_format.image_width  = ovl->ovl_rect.w;
+  tmp_format.image_height = ovl->ovl_rect.h;
+  tmp_format.frame_width  = tmp_format.image_width;
+  tmp_format.frame_height = tmp_format.image_height;
+  
+  gavl_video_frame_get_subframe(spumux->format.pixelformat, ovl->frame, spumux->subframe, &ovl->ovl_rect);
+
+  image_filename = bg_sprintf(spumux->filename_template, spumux->subtitles_written);
+
+  bg_pngwriter_write_header(priv, image_filename,
+                            &tmp_format);
+
+  fprintf(stderr, "Tmp format:\n");
+  gavl_video_format_dump(&tmp_format);
+  
+  bg_pngwriter_write_image(priv, spumux->subframe);
+
+  fprintf(spumux->xml_file, "    <spu start=\"");
+  print_time(spumux->xml_file, ovl->frame->time_scaled, &spumux->format);
+
+  fprintf(spumux->xml_file, "\" end=\"");
+  print_time(spumux->xml_file, ovl->frame->time_scaled+ovl->frame->duration_scaled, &spumux->format);
+  fprintf(spumux->xml_file, "\" xoffset=\"%d\" yoffset=\"%d\" image=\"%s\"/>\n", ovl->dst_x, ovl->dst_y,
+          image_filename);
+  
+  free(image_filename);
+    
+  spumux->subtitles_written++;
+  }
+
+static void close_spumux(void * priv, int do_delete)
+  {
+  int i;
+  spumux_t * spumux = (spumux_t*)priv;
+  char * image_filename;
+
+  fprintf(spumux->xml_file, "  </stream>\n</subpictures>\n");
+  fclose(spumux->xml_file);
+  spumux->xml_file = NULL;
+  
+  if(do_delete)
+    {
+    for(i = 0; i < spumux->subtitles_written; i++)
+      {
+      image_filename = bg_sprintf(spumux->filename_template, spumux->subtitles_written);
+      remove(image_filename);
+      free(image_filename);
+      }
+    remove(spumux->filename);
+    }
+  }
+
+static void destroy_spumux(void * priv)
+  {
+  spumux_t * spumux = (spumux_t*)priv;
+  if(spumux->xml_file)
+    close_spumux(priv, 1);
+  
+  if(spumux->filename) free(spumux->filename);
+  if(spumux->filename_template) free(spumux->filename_template);
+  if(spumux->subframe)
+    {
+    gavl_video_frame_null(spumux->subframe);
+    gavl_video_frame_destroy(spumux->subframe);
+    }
+  free(spumux);
+  }
+
+
+static bg_parameter_info_t parameters[] =
+  {
+    {
+      name:        "compression",
+      long_name:   "Compression level",
+      type:        BG_PARAMETER_SLIDER_INT,
+      val_min:     { val_i: 0 },
+      val_max:     { val_i: 9 },
+      val_default: { val_i: 9 },
+    },
+    { /* End of parameters */ }
+  };
+
+static bg_parameter_info_t * get_parameters_spumux(void * priv)
+  {
+  return parameters;
+  }
+
+
+
+bg_encoder_plugin_t the_plugin =
+  {
+    common:
+    {
+      name:           "e_spumux",       /* Unique short name */
+      long_name:      "spumux overlay exporter",
+      mimetypes:      NULL,
+      extensions:     "xml",
+      type:           BG_PLUGIN_ENCODER_SUBTITLE_OVERLAY,
+      flags:          BG_PLUGIN_FILE,
+      priority:       BG_PLUGIN_PRIORITY_MAX,
+      create:         create_spumux,
+      destroy:        destroy_spumux,
+      get_parameters: get_parameters_spumux,
+      set_parameter:  bg_pngwriter_set_parameter,
+      //   get_error:      get_error_spumux,
+    },
+
+    max_subtitle_overlay_streams: 1,
+    
+    get_extension:        get_extension_spumux,
+    
+    open:                 open_spumux,
+
+    add_subtitle_overlay_stream:     add_subtitle_overlay_stream_spumux,
+    
+    start:                start_spumux,
+
+    get_subtitle_overlay_format: get_subtitle_overlay_format_spumux,
+    
+    write_subtitle_overlay: write_subtitle_overlay_spumux,
+    close:             close_spumux,
+  };
+
+/* Include this into all plugin modules exactly once
+   to let the plugin loader obtain the API version */
+BG_GET_PLUGIN_API_VERSION;
---
+++ gmerlin/plugins/png/pngwriter.c
@@ -0,0 +1,146 @@
+/*****************************************************************
+ 
+  pngwriter.c
+ 
+  Copyright (c) 2003-2006 by Burkhard Plaum - plaum@ipf.uni-stuttgart.de
+ 
+  http://gmerlin.sourceforge.net
+ 
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+ 
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111, USA.
+ 
+*****************************************************************/
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#include <plugin.h>
+#include <utils.h>
+
+#include <png.h>
+
+#include "pngwriter.h"
+
+int bg_pngwriter_write_header(void * priv, const char * filename,
+                              gavl_video_format_t * format)
+  {
+  int color_type;
+  int bits = 8;
+  bg_pngwriter_t * png = (bg_pngwriter_t*)priv;
+
+  png->transform_flags = PNG_TRANSFORM_IDENTITY;
+  
+  png->output = fopen(filename, "wb");
+  if(!png->output)
+    return 0;
+
+  png->png_ptr = png_create_write_struct(PNG_LIBPNG_VER_STRING, NULL,
+                                         NULL, NULL);
+
+  png->info_ptr = png_create_info_struct(png->png_ptr);
+  setjmp(png_jmpbuf(png->png_ptr));
+  png_init_io(png->png_ptr, png->output);
+
+  switch(png->bit_mode)
+    {
+    case BITS_AUTO:
+      /* Decide according to the input format */
+      if(gavl_pixelformat_is_planar(format->pixelformat))
+        {
+        if(gavl_pixelformat_bytes_per_component(format->pixelformat) > 1)
+          bits = 16;
+        }
+      else if(gavl_pixelformat_bytes_per_pixel(format->pixelformat) > 4)
+        bits = 16;
+      break;
+    case BITS_8:
+      bits = 8;
+      break;
+    case BITS_16:
+      bits = 16;
+      break;
+      
+    }
+#ifdef GAVL_PROCESSOR_LITTLE_ENDIAN
+//  fprintf(stderr, "LITTLE ENDIAN\n");
+  if(bits > 8)
+    png->transform_flags |= PNG_TRANSFORM_SWAP_ENDIAN;
+#endif
+  
+  if(gavl_pixelformat_has_alpha(format->pixelformat))
+    {
+    color_type = PNG_COLOR_TYPE_RGBA;
+    if(bits == 8)
+      format->pixelformat = GAVL_RGBA_32;
+    else
+      format->pixelformat = GAVL_RGBA_64;
+    }
+  else
+    {
+    color_type = PNG_COLOR_TYPE_RGB;
+    if(bits == 8)
+      format->pixelformat = GAVL_RGB_24;
+    else
+      format->pixelformat = GAVL_RGB_48;
+    }
+  
+  png_set_compression_level(png->png_ptr, png->compression_level);
+  png_set_IHDR(png->png_ptr, png->info_ptr,
+               format->image_width,
+               format->image_height,
+               bits, color_type, PNG_INTERLACE_NONE,
+               PNG_COMPRESSION_TYPE_DEFAULT, PNG_FILTER_TYPE_DEFAULT);
+  
+  gavl_video_format_copy(&(png->format), format);
+  
+  return 1;
+  }
+
+int bg_pngwriter_write_image(void * priv, gavl_video_frame_t * frame)
+  {
+  int i;
+  unsigned char ** rows;
+  bg_pngwriter_t * png = (bg_pngwriter_t*)priv;
+
+  rows = malloc(png->format.image_height * sizeof(*rows));
+
+  for(i = 0; i < png->format.image_height; i++)
+    rows[i] = frame->planes[0] + i * frame->strides[0];
+
+  png_set_rows(png->png_ptr, png->info_ptr, rows);
+  png_write_png(png->png_ptr, png->info_ptr, png->transform_flags, NULL);
+ 
+  png_destroy_write_struct(&png->png_ptr, &png->info_ptr);
+  fclose(png->output);
+  free(rows);
+  return 1;
+  }
+
+void bg_pngwriter_set_parameter(void * p, char * name,
+                                bg_parameter_value_t * val)
+  {
+  bg_pngwriter_t * png;
+  png = (bg_pngwriter_t *)p;
+  
+  if(!name)
+    return;
+
+  if(!strcmp(name, "compression"))
+    png->compression_level = val->val_i;
+  if(!strcmp(name, "bit_mode"))
+    {
+    //    fprintf(stderr, "SET BIT MODE: %s\n", val->val_str);
+    if(!strcmp(val->val_str, "Auto"))
+      png->bit_mode = BITS_AUTO;
+    else
+      png->bit_mode = atoi(val->val_str);
+    }
+
+  }
