7eb6ea308e00f7496a44e85752d27e97a550d548
gmerlin <gmerlin@214d8ac5-8513-4f59-80f8-2f39b674a24a>
2006-12-20 00:03:09 +0000

    * Null media header (ROTFL!)
    git-svn-id: svn://svn.code.sf.net/p/gmerlin/code/trunk@1082 214d8ac5-8513-4f59-80f8-2f39b674a24a

---
+++ gmerlin_avdecoder/lib/qt_nmhd.c
@@ -0,0 +1,41 @@
+/*****************************************************************
+ 
+  qt_nmhd.c
+ 
+  Copyright (c) 2003-2006 by Burkhard Plaum - plaum@ipf.uni-stuttgart.de
+ 
+  http://gmerlin.sourceforge.net
+ 
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+ 
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111, USA.
+ 
+*****************************************************************/
+
+#include <avdec_private.h>
+#include <qt.h>
+
+int bgav_qt_nmhd_read(qt_atom_header_t * h, bgav_input_context_t * input,
+                      qt_nmhd_t * ret)
+  {
+  READ_VERSION_AND_FLAGS;
+  }
+
+void bgav_qt_nmhd_free(qt_nmhd_t * g)
+  {
+
+  }
+
+void bgav_qt_nmhd_dump(int indent, qt_nmhd_t * g)
+  {
+  bgav_diprintf(indent, "nmhd\n");
+
+  bgav_diprintf(indent+2, "version:       %d\n", g->version);
+  bgav_diprintf(indent+2, "flags:         %08x\n", g->flags);
+  bgav_diprintf(indent, "end of nmhd\n");
+  }
