1cf3337a3a29006ebbdcf69d4155d99a69cfa91d
gmerlin <gmerlin@214d8ac5-8513-4f59-80f8-2f39b674a24a>
2007-05-03 12:52:12 +0000

    * AI_NUMERICSERV -> AI_NUMERICHOST
    git-svn-id: svn://svn.code.sf.net/p/gmerlin/code/trunk@1198 214d8ac5-8513-4f59-80f8-2f39b674a24a

--- gmerlin/lib/socket.c
+++ gmerlin/lib/socket.c
@@ -193,7 +193,7 @@ static struct addrinfo * hostbyname(const char * hostname, int port, int socktyp
   /* prevent DNS lookup for numeric IP addresses */
 
   if(inet_aton(hostname, &(ipv4_addr)))
-    hints.ai_flags |= AI_NUMERICSERV;
+    hints.ai_flags |= AI_NUMERICHOST;
   if((err = getaddrinfo(hostname, (char*)0 /* service */,
                         &hints, &ret)))
     {
