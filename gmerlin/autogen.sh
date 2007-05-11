3ca17f0fd2dbc28a3dc5d87351ea91b9901576d0
gmerlin <gmerlin@214d8ac5-8513-4f59-80f8-2f39b674a24a>
2007-05-11 12:03:52 +0000

    * Check for glibtoolize and exit if something failed
    git-svn-id: svn://svn.code.sf.net/p/gmerlin/code/trunk@1204 214d8ac5-8513-4f59-80f8-2f39b674a24a

--- gmerlin/autogen.sh
+++ gmerlin/autogen.sh
@@ -1,8 +1,65 @@
 #!/bin/sh
 ./make_potfiles
-aclocal -I m4
-libtoolize --automake --copy --force
+if test -d /usr/local/share/aclocal; then
+ACLOCAL_FLAGS="-I /usr/local/share/aclocal"
+else
+ACLOCAL_FLAGS=""
+fi
+
+LIBTOOLIZE=`which glibtoolize`
+if test "x$LIBTOOLIZE" = "x"; then
+  LIBTOOLIZE=`which libtoolize`
+fi
+
+if test "x$LIBTOOLIZE" = "x"; then
+  echo "No libtoolize command found"
+  exit 1
+fi
+
+echo -n "doing aclocal..."
+aclocal $ACLOCAL_FLAGS -I m4
+
+if test $? != "0"; then
+  echo "failed"
+  exit 1
+fi
+
+echo "done"
+
+
+echo -n "doing libtoolize..."
+$LIBTOOLIZE --automake --copy --force
+
+if test $? != "0"; then
+  echo "failed"
+  exit 1
+fi
+
+echo "done"
+echo -n "doing autoheader..."
 autoheader
+
+if test $? != "0"; then
+  echo "failed"
+  exit 1
+fi
+
+echo "done"
+echo -n "doing autoconf..."
 autoconf
+
+if test $? != "0"; then
+  echo "failed"
+  exit 1
+fi
+
+echo "done"
+echo -n "doing automake..."
 automake -a
+if test $? != "0"; then
+  echo "failed"
+  exit 1
+fi
+echo "done"
+
 echo "You can now run ./configure"
