ad4bcf5bee64105f28101a93b72f07b2fb30af59
gmerlin <gmerlin@214d8ac5-8513-4f59-80f8-2f39b674a24a>
2007-10-18 12:53:46 +0000

    * Nicer output
    git-svn-id: svn://svn.code.sf.net/p/gmerlin/code/trunk@1281 214d8ac5-8513-4f59-80f8-2f39b674a24a

--- gavl/src/pixelformat_penalty.c
+++ gavl/src/pixelformat_penalty.c
@@ -13,7 +13,7 @@ int main(int argc, char ** argv)
     for(j = 0; j < num; j++)
       {
       dst = gavl_get_pixelformat(j);
-      fprintf(stderr, "src: %23s, dst: %23s, penalty: %6d\n",
+      fprintf(stderr, "src: %-23s, dst: %-23s, penalty: %6d\n",
               gavl_pixelformat_to_string(src),
               gavl_pixelformat_to_string(dst),
               gavl_pixelformat_conversion_penalty(src, dst));
