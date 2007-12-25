5fd36108dc465ea1c9acbfdc1cfa39593bcdc4ef
gmerlin <gmerlin@214d8ac5-8513-4f59-80f8-2f39b674a24a>
2007-12-25 15:57:30 +0000

    * Merged 2 equalizer plugins into one
    git-svn-id: svn://svn.code.sf.net/p/gmerlin/code/trunk@1431 214d8ac5-8513-4f59-80f8-2f39b674a24a

--- gmerlin/plugins/videofilters/fv_equalizer.c
+++ gmerlin/plugins/videofilters/fv_equalizer.c
@@ -39,13 +39,18 @@
 
 #define LOG_DOMAIN "fv_equalizer"
 
-typedef struct
+#include "colormatrix.h"
+
+typedef struct equalizer_priv_s
   {
   int brightness;
   int contrast;
   float hue;
   float saturation;
 
+  bg_colormatrix_t * mat;
+  float coeffs[3][4];
+  
   /* Offsets and advance */
   int offset[3];
   int advance[2];
@@ -56,8 +61,11 @@ typedef struct
 
   int chroma_width;
   int chroma_height;
+  int use_matrix;
   
   gavl_video_format_t format;
+
+  gavl_video_frame_t * frame;
   
   void (*process_bc)(unsigned char *dest, int dstride,
                      int w, int h, int brightness, int contrast, int advance);
@@ -66,7 +74,43 @@ typedef struct
                      int dststride,
                      int w, int h, float hue, float sat, int advance);
   
+  int (*read_video)(struct equalizer_priv_s *,
+                    gavl_video_frame_t * frame);
+  
   } equalizer_priv_t;
+
+static void set_coeffs(equalizer_priv_t * vp)
+  {
+  float s, c;
+  float contrast, brightness;
+  
+  /* Brightness and contrast go to Y */
+
+  contrast = (vp->contrast+100.0) / 100.0;
+  brightness = (vp->brightness+100.0)/100.0 - 0.5 * (1.0 + contrast);
+  
+  vp->coeffs[0][0] = contrast;
+  vp->coeffs[0][1] = 0.0;
+  vp->coeffs[0][2] = 0.0;
+  vp->coeffs[0][3] = brightness;
+  
+  /* Saturation and Hue go to U and V */
+
+  s = sin(vp->hue) * vp->saturation;
+  c = cos(vp->hue) * vp->saturation;
+
+  vp->coeffs[1][0] = 0.0;
+  vp->coeffs[1][1] = c;
+  vp->coeffs[1][2] = -s;
+  vp->coeffs[1][3] = 0.0;
+
+  vp->coeffs[2][0] = 0.0;
+  vp->coeffs[2][1] = s;
+  vp->coeffs[2][2] = c;
+  vp->coeffs[2][3] = 0.0;
+  
+  }
+
 
 static void process_sh_C(uint8_t *udst, uint8_t *vdst,
                          int dststride, 
@@ -103,6 +147,29 @@ static void process_bc_C(unsigned char *dest, int dstride,
   int pel;
   
   contrast = ((contrast+100)*256*256)/100;
+  brightness = (((brightness+100)*511*219)/200-128*219 - (contrast*219)/512) / 255;
+  
+  while (h--)
+    {
+    index = 0;
+    for (i = w; i; i--)
+      {
+      pel = ((dest[index] * contrast)>>16) + brightness;
+      if(pel&768) pel = (-pel)>>31;
+      dest[index] = pel;
+      index += advance;
+      }
+    dest += dstride;
+    }
+  }
+
+static void process_bcj_C(unsigned char *dest, int dstride,
+                          int w, int h, int brightness, int contrast, int advance)
+  {
+  int i, index;
+  int pel;
+  
+  contrast = ((contrast+100)*256*256)/100;
   brightness = ((brightness+100)*511)/200-128 - contrast/512;
   
   while (h--)
@@ -119,10 +186,134 @@ static void process_bc_C(unsigned char *dest, int dstride,
     }
   }
 
+/* 16 bit YUV */
+
+static void process_sh16_C(uint8_t *udst1, uint8_t *vdst1,
+                           int dststride, 
+                           int w, int h, float hue, float sat, int advance)
+  {
+  int i, index;
+  const int64_t s= rint(sin(hue) * (1<<16) * sat);
+  const int64_t c= rint(cos(hue) * (1<<16) * sat);
+  uint16_t * udst;
+  uint16_t * vdst;
+  
+  while (h--)
+    {
+    udst = (uint16_t *)udst1;
+    vdst = (uint16_t *)vdst1;
+    
+    index = 0;
+    for (i = 0; i<w; i++)
+      {
+      const int64_t u= udst[index] - (128<<8);
+      const int64_t v= vdst[index] - (128<<8);
+      int64_t new_u= (c*u - s*v + (1<<(15+8)) + (128LL<<(16+8)))>>16;
+      int64_t new_v= (s*u + c*v + (1<<(15+8)) + (128LL<<(16+8)))>>16;
+      if(new_u & 0xFFFF0000LL) new_u= (-new_u)>>63;
+      if(new_v & 0xFFFF0000LL) new_v= (-new_v)>>63;
+      udst[index]= new_u;
+      vdst[index]= new_v;
+      index += advance;
+      }
+    udst1 += dststride;
+    vdst1 += dststride;
+    }
+  }
+
+static void process_bc16_C(unsigned char *dest1, int dstride,
+                           int w, int h, int brightness1, int contrast1, int advance)
+  {
+  int i, index;
+  int64_t pel;
+  uint16_t * dest;
+  int64_t brightness, contrast;
+  
+  contrast = ((contrast1+100)*256*256)/100;
+  brightness = ((brightness1+100)*511*219)/200-128*219 - (contrast*219)/512;
+  brightness = (brightness * 0x0101)/255;
+  
+  while (h--)
+    {
+    dest = (uint16_t *)dest1;
+    
+    index = 0;
+    for (i = w; i; i--)
+      {
+      pel = (((int64_t)dest[index] * contrast)>>16) + brightness;
+      if(pel & 0xFFFF0000LL) pel= (-pel)>>63;
+      dest[index] = pel;
+      index += advance;
+      }
+    dest1 += dstride;
+    }
+  }
+
+
+
+static int read_video_fast(equalizer_priv_t * vp,
+                           gavl_video_frame_t * frame)
+  {
+  if(!vp->read_func(vp->read_data, frame, vp->read_stream))
+    return 0;
+
+  /* Do the conversion */
+
+  if((vp->contrast != 0.0) || (vp->brightness != 0.0))
+    {
+    vp->process_bc(frame->planes[0] + vp->offset[0], frame->strides[0],
+                   vp->format.image_width,
+                   vp->format.image_height,
+                   vp->brightness, vp->contrast, vp->advance[0]);
+    }
+  
+  if((vp->hue != 0.0) || (vp->saturation != 1.0))
+    {
+    if(gavl_pixelformat_is_planar(vp->format.pixelformat))
+      {
+      vp->process_sh(frame->planes[1] + vp->offset[1],
+                     frame->planes[2] + vp->offset[2],
+                     frame->strides[1],
+                     vp->chroma_width,
+                     vp->chroma_height,
+                     vp->hue, vp->saturation, vp->advance[1]);
+      }
+    else
+      {
+      vp->process_sh(frame->planes[0] + vp->offset[1],
+                     frame->planes[0] + vp->offset[2],
+                     frame->strides[0],
+                     vp->chroma_width,
+                     vp->chroma_height,
+                     vp->hue, vp->saturation, vp->advance[1]);
+      }
+    }
+  
+  return 1;
+  }
+
+static int read_video_matrix(equalizer_priv_t * vp,
+                             gavl_video_frame_t * frame)
+  {
+  if(!vp->frame)
+    {
+    vp->frame = gavl_video_frame_create(&vp->format);
+    gavl_video_frame_clear(vp->frame, &vp->format);
+    }
+  if(!vp->read_func(vp->read_data, vp->frame, vp->read_stream))
+    return 0;
+  
+  bg_colormatrix_process(vp->mat, vp->frame, frame);
+  frame->timestamp = vp->frame->timestamp;
+  frame->duration = vp->frame->duration;
+  return 1;
+  }
+
 static void * create_equalizer()
   {
   equalizer_priv_t * ret;
   ret = calloc(1, sizeof(*ret));
+  ret->mat = bg_colormatrix_create();
   return ret;
   }
 
@@ -130,6 +321,9 @@ static void destroy_equalizer(void * priv)
   {
   equalizer_priv_t * vp;
   vp = (equalizer_priv_t *)priv;
+  if(vp->frame)
+    gavl_video_frame_destroy(vp->frame);
+  bg_colormatrix_destroy(vp->mat);
   free(vp);
   }
 
@@ -188,20 +382,58 @@ static bg_parameter_info_t * get_parameters_equalizer(void * priv)
 static void set_parameter_equalizer(void * priv, const char * name,
                                     const bg_parameter_value_t * val)
   {
+  int changed = 0;
   equalizer_priv_t * vp;
-  vp = (equalizer_priv_t *)priv;
+  int test_i;
+  float test_f;
 
+  vp = (equalizer_priv_t *)priv;
+  
   if(!name)
     return;
-
+  
   if(!strcmp(name, "brightness"))
-    vp->brightness = (int)(10*val->val_f);
+    {
+    test_i = 10*val->val_f;
+    if(vp->brightness != test_i)
+      {
+      vp->brightness = test_i;
+      changed = 1;
+      }
+    }
   else if(!strcmp(name, "contrast"))
-    vp->contrast = (int)(10*val->val_f);
+    {
+    test_i = 10*val->val_f;
+    if(vp->contrast != test_i)
+      {
+      vp->contrast = test_i;
+      changed = 1;
+      }
+    }
   else if(!strcmp(name, "saturation"))
-    vp->saturation = (val->val_f + 10.0)/10.0;
+    {
+    test_f = (val->val_f + 10.0)/10.0;
+    
+    if(vp->saturation != test_f)
+      {
+      vp->saturation = test_f;
+      changed = 1;
+      }
+    }
   else if(!strcmp(name, "hue"))
-    vp->hue = val->val_f * M_PI / 180.0;
+    {
+    test_f = val->val_f * M_PI / 180.0;
+    if(vp->hue != test_f)
+      {
+      vp->hue = test_f;
+      changed = 1;
+      }
+    }
+  if(changed && vp->use_matrix)
+    {
+    set_coeffs(vp);
+    bg_colormatrix_set_yuv(vp->mat, vp->coeffs);
+    }
   }
 
 static void connect_input_port_equalizer(void * priv,
@@ -220,93 +452,124 @@ static void connect_input_port_equalizer(void * priv,
   
   }
 
+
+
 static void set_input_format_equalizer(void * priv, gavl_video_format_t * format, int port)
   {
   int sub_h, sub_v;
   equalizer_priv_t * vp;
   vp = (equalizer_priv_t *)priv;
 
-  if(!port)
-    {
-    switch(format->pixelformat)
-      {
-      case GAVL_RGB_15:
-      case GAVL_BGR_15:
-      case GAVL_RGB_16:
-      case GAVL_BGR_16:
-      case GAVL_RGB_24:
-      case GAVL_BGR_24:
-      case GAVL_RGB_32:
-      case GAVL_BGR_32:
-      case GAVL_YUV_444_P:
-      case GAVL_YUV_444_P_16:
-      case GAVL_RGB_48:
-      case GAVL_RGB_FLOAT:
-        format->pixelformat = GAVL_YUV_444_P;
-        vp->advance[0] = 1;
-        vp->advance[1] = 1;
-        vp->offset[0]  = 0;
-        vp->offset[1]  = 0;
-        vp->offset[2]  = 0;
-        break;
-      case GAVL_RGBA_32:
-      case GAVL_YUVA_32:
-      case GAVL_RGBA_64:
-      case GAVL_RGBA_FLOAT:
-        format->pixelformat = GAVL_YUVA_32;
-        vp->advance[0] = 4;
-        vp->advance[1] = 4;
-        vp->offset[0]  = 0;
-        vp->offset[1]  = 1;
-        vp->offset[2]  = 2;
-        break;
-      case GAVL_YUY2:
-        vp->advance[0] = 2;
-        vp->advance[1] = 4;
-        vp->offset[0]  = 0;
-        vp->offset[1]  = 1;
-        vp->offset[2]  = 3;
-        break;
-      case GAVL_UYVY:
-        vp->advance[0] = 2;
-        vp->advance[1] = 4;
-        vp->offset[0]  = 1;
-        vp->offset[1]  = 0;
-        vp->offset[2]  = 2;
-        break;
-      case GAVL_YUV_420_P:
-      case GAVL_YUV_410_P:
-      case GAVL_YUV_411_P:
-      case GAVL_YUVJ_420_P:
-      case GAVL_YUVJ_422_P:
-      case GAVL_YUVJ_444_P:
-        vp->advance[0] = 1;
-        vp->advance[1] = 1;
-        vp->offset[0]  = 0;
-        vp->offset[1]  = 0;
-        vp->offset[2]  = 0;
-        break;
-      case GAVL_YUV_422_P:
-      case GAVL_YUV_422_P_16:
-        format->pixelformat = GAVL_YUV_422_P;
-        vp->advance[0] = 1;
-        vp->advance[1] = 1;
-        vp->offset[0]  = 0;
-        vp->offset[1]  = 0;
-        vp->offset[2]  = 0;
-        break;
-      case GAVL_PIXELFORMAT_NONE:
-        break;
-      }
-    gavl_video_format_copy(&vp->format, format);
-    }
   vp->process_bc = process_bc_C;
   vp->process_sh = process_sh_C;
+  vp->use_matrix = 0;
 
-  gavl_pixelformat_chroma_sub(format->pixelformat, &sub_h, &sub_v);
+  if(port)
+    return;
+  
+  switch(format->pixelformat)
+    {
+    case GAVL_RGB_15:
+    case GAVL_BGR_15:
+    case GAVL_RGB_16:
+    case GAVL_BGR_16:
+    case GAVL_RGB_24:
+    case GAVL_BGR_24:
+    case GAVL_RGB_32:
+    case GAVL_BGR_32:
+    case GAVL_RGB_48:
+    case GAVL_RGB_FLOAT:
+    case GAVL_RGBA_32:
+    case GAVL_RGBA_64:
+    case GAVL_RGBA_FLOAT:
+      vp->use_matrix = 1;
+      break;
+    case GAVL_YUV_444_P:
+      vp->advance[0] = 1;
+      vp->advance[1] = 1;
+      vp->offset[0]  = 0;
+      vp->offset[1]  = 0;
+      vp->offset[2]  = 0;
+      break;
+    case GAVL_YUVA_32:
+      format->pixelformat = GAVL_YUVA_32;
+      vp->advance[0] = 4;
+      vp->advance[1] = 4;
+      vp->offset[0]  = 0;
+      vp->offset[1]  = 1;
+      vp->offset[2]  = 2;
+      break;
+    case GAVL_YUY2:
+      vp->advance[0] = 2;
+      vp->advance[1] = 4;
+      vp->offset[0]  = 0;
+      vp->offset[1]  = 1;
+      vp->offset[2]  = 3;
+      break;
+    case GAVL_UYVY:
+      vp->advance[0] = 2;
+      vp->advance[1] = 4;
+      vp->offset[0]  = 1;
+      vp->offset[1]  = 0;
+      vp->offset[2]  = 2;
+      break;
+    case GAVL_YUV_444_P_16:
+    case GAVL_YUV_422_P_16:
+      vp->process_bc = process_bc16_C;
+      vp->process_sh = process_sh16_C;
+      /* Fall thrhough */
+    case GAVL_YUV_420_P:
+    case GAVL_YUV_410_P:
+    case GAVL_YUV_411_P:
+    case GAVL_YUV_422_P:
+      vp->advance[0] = 1;
+      vp->advance[1] = 1;
+      vp->offset[0]  = 0;
+      vp->offset[1]  = 0;
+      vp->offset[2]  = 0;
+      break;
+    case GAVL_YUVJ_420_P:
+    case GAVL_YUVJ_422_P:
+    case GAVL_YUVJ_444_P:
+      vp->process_bc = process_bcj_C;
+      vp->advance[0] = 1;
+      vp->advance[1] = 1;
+      vp->offset[0]  = 0;
+      vp->offset[1]  = 0;
+      vp->offset[2]  = 0;
+      break;
+    case GAVL_PIXELFORMAT_NONE:
+      break;
+    }
 
-  vp->chroma_width  = format->image_width / sub_h;
-  vp->chroma_height = format->image_height / sub_v;
+  if(vp->use_matrix)
+    {
+    set_coeffs(vp);
+    
+    bg_colormatrix_init(vp->mat, format, 0);
+    bg_colormatrix_set_yuv(vp->mat, vp->coeffs);
+    vp->read_video = read_video_matrix;
+    }
+  else
+    {
+    gavl_pixelformat_chroma_sub(format->pixelformat, &sub_h, &sub_v);
+    
+    vp->chroma_width  = format->image_width / sub_h;
+    vp->chroma_height = format->image_height / sub_v;
+    vp->read_video = read_video_fast;
+    }
+
+  if(vp->frame)
+    {
+    gavl_video_frame_destroy(vp->frame);
+    vp->frame = (gavl_video_frame_t*)0;
+    }
+
+  bg_log(BG_LOG_DEBUG, LOG_DOMAIN, "Pixelformat: %s",
+         TRD(gavl_pixelformat_to_string(format->pixelformat), NULL));
+  
+  gavl_video_format_copy(&vp->format, format);
+  
   }
 
 static void get_output_format_equalizer(void * priv, gavl_video_format_t * format)
@@ -316,47 +579,13 @@ static void get_output_format_equalizer(void * priv, gavl_video_format_t * forma
   gavl_video_format_copy(format, &vp->format);
   }
 
-static int read_video_equalizer(void * priv, gavl_video_frame_t * frame, int stream)
+
+static int
+read_video_equalizer(void * priv, gavl_video_frame_t * frame, int stream)
   {
   equalizer_priv_t * vp;
   vp = (equalizer_priv_t *)priv;
-  
-  if(!vp->read_func(vp->read_data, frame, vp->read_stream))
-    return 0;
-
-  /* Do the conversion */
-
-  if((vp->contrast != 0.0) || (vp->brightness != 0.0))
-    {
-    vp->process_bc(frame->planes[0] + vp->offset[0], frame->strides[0],
-                   vp->format.image_width,
-                   vp->format.image_height,
-                   vp->brightness, vp->contrast, vp->advance[0]);
-    }
-  
-  if((vp->hue != 0.0) || (vp->saturation != 1.0))
-    {
-    if(gavl_pixelformat_is_planar(vp->format.pixelformat))
-      {
-      vp->process_sh(frame->planes[1] + vp->offset[1],
-                     frame->planes[2] + vp->offset[2],
-                     frame->strides[1],
-                     vp->chroma_width,
-                     vp->chroma_height,
-                     vp->hue, vp->saturation, vp->advance[1]);
-      }
-    else
-      {
-      vp->process_sh(frame->planes[0] + vp->offset[1],
-                     frame->planes[0] + vp->offset[2],
-                     frame->strides[0],
-                     vp->chroma_width,
-                     vp->chroma_height,
-                     vp->hue, vp->saturation, vp->advance[1]);
-      }
-    }
-  
-  return 1;
+  return vp->read_video(vp, frame);
   }
 
 bg_fv_plugin_t the_plugin = 
@@ -365,8 +594,8 @@ bg_fv_plugin_t the_plugin =
     {
       BG_LOCALE,
       name:      "fv_equalizer",
-      long_name: TRS("Simple yuv equalizer"),
-      description: TRS("Control hue, saturation, contrast and brightness. Based on the vf_eq and vf_hue from the MPlayer project. This is fast and works for all 8 bit Y'CbCr formats. A more generic equalizer (which works in RGB as well) is the Matrix Equalizer"),
+      long_name: TRS("Equalizer"),
+      description: TRS("Control hue, saturation, contrast and brightness. Based on the vf_eq and vf_hue from the MPlayer project. This is fast and works for all 8 bit Y'CbCr formats."),
       type:     BG_PLUGIN_FILTER_VIDEO,
       flags:    BG_PLUGIN_FILTER_1,
       create:   create_equalizer,
