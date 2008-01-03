3168c3b5496e1d35c57afbf2f4a6e9c9acbae44c
gmerlin <gmerlin@214d8ac5-8513-4f59-80f8-2f39b674a24a>
2008-01-03 00:08:38 +0000

    * Make pixelformat conversion tables const
    git-svn-id: svn://svn.code.sf.net/p/gmerlin/code/trunk@1443 214d8ac5-8513-4f59-80f8-2f39b674a24a

--- gavl/gavl/c/colorspace_tables.h
+++ gavl/gavl/c/colorspace_tables.h
@@ -1,70 +1,50 @@
-/*****************************************************************
- * gavl - a general purpose audio/video processing library
- *
- * Copyright (c) 2001 - 2008 Members of the Gmerlin project
- * gmerlin-general@lists.sourceforge.net
- * http://gmerlin.sourceforge.net
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- * *****************************************************************/
-extern uint8_t gavl_yj_8_to_y_8[256];
-extern uint8_t gavl_uvj_8_to_uv_8[256];
-extern uint16_t gavl_yj_8_to_y_16[256];
-extern uint16_t gavl_uvj_8_to_uv_16[256];
-extern uint8_t gavl_y_8_to_yj_8[256];
-extern uint8_t gavl_uv_8_to_uvj_8[256];
-extern uint8_t gavl_rgb_5_to_8[32];
-extern uint8_t gavl_rgb_6_to_8[64];
-extern uint16_t gavl_rgb_5_to_16[32];
-extern uint16_t gavl_rgb_6_to_16[64];
-extern float gavl_rgb_5_to_float[32];
-extern float gavl_rgb_6_to_float[64];
-extern int gavl_r_to_y[256];
-extern int gavl_g_to_y[256];
-extern int gavl_b_to_y[256];
-extern int gavl_r_to_u[256];
-extern int gavl_g_to_u[256];
-extern int gavl_b_to_u[256];
-extern int gavl_r_to_v[256];
-extern int gavl_g_to_v[256];
-extern int gavl_b_to_v[256];
-extern int gavl_r_to_yj[256];
-extern int gavl_g_to_yj[256];
-extern int gavl_b_to_yj[256];
-extern int gavl_r_to_uj[256];
-extern int gavl_g_to_uj[256];
-extern int gavl_b_to_uj[256];
-extern int gavl_r_to_vj[256];
-extern int gavl_g_to_vj[256];
-extern int gavl_b_to_vj[256];
-extern int gavl_y_to_rgb[256];
-extern int gavl_v_to_r[256];
-extern int gavl_u_to_g[256];
-extern int gavl_v_to_g[256];
-extern int gavl_u_to_b[256];
-extern int gavl_yj_to_rgb[256];
-extern int gavl_vj_to_r[256];
-extern int gavl_uj_to_g[256];
-extern int gavl_vj_to_g[256];
-extern int gavl_uj_to_b[256];
-extern float gavl_y_to_rgb_float[256];
-extern float gavl_v_to_r_float[256];
-extern float gavl_u_to_g_float[256];
-extern float gavl_v_to_g_float[256];
-extern float gavl_u_to_b_float[256];
-extern float gavl_yj_to_rgb_float[256];
-extern float gavl_vj_to_r_float[256];
-extern float gavl_uj_to_g_float[256];
-extern float gavl_vj_to_g_float[256];
-extern float gavl_uj_to_b_float[256];
+extern const uint8_t gavl_yj_8_to_y_8[256];
+extern const uint8_t gavl_uvj_8_to_uv_8[256];
+extern const uint16_t gavl_yj_8_to_y_16[256];
+extern const uint16_t gavl_uvj_8_to_uv_16[256];
+extern const uint8_t gavl_y_8_to_yj_8[256];
+extern const uint8_t gavl_uv_8_to_uvj_8[256];
+extern const uint8_t gavl_rgb_5_to_8[32];
+extern const uint8_t gavl_rgb_6_to_8[64];
+extern const uint16_t gavl_rgb_5_to_16[32];
+extern const uint16_t gavl_rgb_6_to_16[64];
+extern const float gavl_rgb_5_to_float[32];
+extern const float gavl_rgb_6_to_float[64];
+extern const int gavl_r_to_y[256];
+extern const int gavl_g_to_y[256];
+extern const int gavl_b_to_y[256];
+extern const int gavl_r_to_u[256];
+extern const int gavl_g_to_u[256];
+extern const int gavl_b_to_u[256];
+extern const int gavl_r_to_v[256];
+extern const int gavl_g_to_v[256];
+extern const int gavl_b_to_v[256];
+extern const int gavl_r_to_yj[256];
+extern const int gavl_g_to_yj[256];
+extern const int gavl_b_to_yj[256];
+extern const int gavl_r_to_uj[256];
+extern const int gavl_g_to_uj[256];
+extern const int gavl_b_to_uj[256];
+extern const int gavl_r_to_vj[256];
+extern const int gavl_g_to_vj[256];
+extern const int gavl_b_to_vj[256];
+extern const int gavl_y_to_rgb[256];
+extern const int gavl_v_to_r[256];
+extern const int gavl_u_to_g[256];
+extern const int gavl_v_to_g[256];
+extern const int gavl_u_to_b[256];
+extern const int gavl_yj_to_rgb[256];
+extern const int gavl_vj_to_r[256];
+extern const int gavl_uj_to_g[256];
+extern const int gavl_vj_to_g[256];
+extern const int gavl_uj_to_b[256];
+extern const float gavl_y_to_rgb_float[256];
+extern const float gavl_v_to_r_float[256];
+extern const float gavl_u_to_g_float[256];
+extern const float gavl_v_to_g_float[256];
+extern const float gavl_u_to_b_float[256];
+extern const float gavl_yj_to_rgb_float[256];
+extern const float gavl_vj_to_r_float[256];
+extern const float gavl_uj_to_g_float[256];
+extern const float gavl_vj_to_g_float[256];
+extern const float gavl_uj_to_b_float[256];
--- gavl/src/gen_colorspace_tables.c
+++ gavl/src/gen_colorspace_tables.c
@@ -61,8 +61,8 @@ int main(int argc, char ** argv)
   /* JPEG Quantisation <-> MPEG Quantisation */
     
   /* yj_8 -> y_8 */
-  fprintf(header, "extern uint8_t gavl_yj_8_to_y_8[256];\n");
-  fprintf(source, "uint8_t gavl_yj_8_to_y_8[256] = \n{\n");
+  fprintf(header, "extern const uint8_t gavl_yj_8_to_y_8[256];\n");
+  fprintf(source, "const uint8_t gavl_yj_8_to_y_8[256] = \n{\n");
   for(i = 0; i < 256; i++)
     {
     if(!((i)%8))
@@ -76,8 +76,8 @@ int main(int argc, char ** argv)
   fprintf(source, "};\n\n");
 
   /* uvj_8 -> uv_8 */
-  fprintf(header, "extern uint8_t gavl_uvj_8_to_uv_8[256];\n");
-  fprintf(source, "uint8_t gavl_uvj_8_to_uv_8[256] = \n{\n");
+  fprintf(header, "extern const uint8_t gavl_uvj_8_to_uv_8[256];\n");
+  fprintf(source, "const uint8_t gavl_uvj_8_to_uv_8[256] = \n{\n");
   for(i = 0; i < 256; i++)
     {
     if(!((i)%8))
@@ -94,8 +94,8 @@ int main(int argc, char ** argv)
   /* yj_8 -> y_16 */
 
 
-  fprintf(header, "extern uint16_t gavl_yj_8_to_y_16[256];\n");
-  fprintf(source, "uint16_t gavl_yj_8_to_y_16[256] = \n{\n");
+  fprintf(header, "extern const uint16_t gavl_yj_8_to_y_16[256];\n");
+  fprintf(source, "const uint16_t gavl_yj_8_to_y_16[256] = \n{\n");
   for(i = 0; i < 256; i++)
     {
     if(!((i)%8))
@@ -109,8 +109,8 @@ int main(int argc, char ** argv)
   fprintf(source, "};\n\n");
 
   /* uvj_8 -> uv_16 */
-  fprintf(header, "extern uint16_t gavl_uvj_8_to_uv_16[256];\n");
-  fprintf(source, "uint16_t gavl_uvj_8_to_uv_16[256] = \n{\n");
+  fprintf(header, "extern const uint16_t gavl_uvj_8_to_uv_16[256];\n");
+  fprintf(source, "const uint16_t gavl_uvj_8_to_uv_16[256] = \n{\n");
 
   for(i = 0; i < 256; i++)
     {
@@ -128,8 +128,8 @@ int main(int argc, char ** argv)
   /* y_8 -> yj_8 */
 
 
-  fprintf(header, "extern uint8_t gavl_y_8_to_yj_8[256];\n");
-  fprintf(source, "uint8_t gavl_y_8_to_yj_8[256] = \n{\n");
+  fprintf(header, "extern const uint8_t gavl_y_8_to_yj_8[256];\n");
+  fprintf(source, "const uint8_t gavl_y_8_to_yj_8[256] = \n{\n");
   for(i = 0; i < 256; i++)
     {
     if(!((i)%8))
@@ -144,8 +144,8 @@ int main(int argc, char ** argv)
   fprintf(source, "};\n\n");
 
   /* uvj_8 -> uv_8 */
-  fprintf(header, "extern uint8_t gavl_uv_8_to_uvj_8[256];\n");
-  fprintf(source, "uint8_t gavl_uv_8_to_uvj_8[256] = \n{\n");
+  fprintf(header, "extern const uint8_t gavl_uv_8_to_uvj_8[256];\n");
+  fprintf(source, "const uint8_t gavl_uv_8_to_uvj_8[256] = \n{\n");
   for(i = 0; i < 256; i++)
     {
     if(!((i)%8))
@@ -163,8 +163,8 @@ int main(int argc, char ** argv)
   /* RGB 5/6 bit -> 8 bit */
 
     
-  fprintf(header, "extern uint8_t gavl_rgb_5_to_8[32];\n");
-  fprintf(source, "uint8_t gavl_rgb_5_to_8[32] = \n{\n");
+  fprintf(header, "extern const uint8_t gavl_rgb_5_to_8[32];\n");
+  fprintf(source, "const uint8_t gavl_rgb_5_to_8[32] = \n{\n");
   for(i = 0; i < 32; i++)
     {
     if(!((i)%8))
@@ -177,8 +177,8 @@ int main(int argc, char ** argv)
     }
   fprintf(source, "};\n\n");
 
-  fprintf(header, "extern uint8_t gavl_rgb_6_to_8[64];\n");
-  fprintf(source, "uint8_t gavl_rgb_6_to_8[64] = \n{\n");
+  fprintf(header, "extern const uint8_t gavl_rgb_6_to_8[64];\n");
+  fprintf(source, "const uint8_t gavl_rgb_6_to_8[64] = \n{\n");
   for(i = 0; i < 64; i++)
     {
     if(!((i)%8))
@@ -195,8 +195,8 @@ int main(int argc, char ** argv)
   /* RGB 5/6 bit -> 16 bit */
 
 
-  fprintf(header, "extern uint16_t gavl_rgb_5_to_16[32];\n");
-  fprintf(source, "uint16_t gavl_rgb_5_to_16[32] = \n{\n");
+  fprintf(header, "extern const uint16_t gavl_rgb_5_to_16[32];\n");
+  fprintf(source, "const uint16_t gavl_rgb_5_to_16[32] = \n{\n");
   for(i = 0; i < 32; i++)
     {
     if(!((i)%8))
@@ -209,8 +209,8 @@ int main(int argc, char ** argv)
     }
   fprintf(source, "};\n\n");
 
-  fprintf(header, "extern uint16_t gavl_rgb_6_to_16[64];\n");
-  fprintf(source, "uint16_t gavl_rgb_6_to_16[64] = \n{\n");
+  fprintf(header, "extern const uint16_t gavl_rgb_6_to_16[64];\n");
+  fprintf(source, "const uint16_t gavl_rgb_6_to_16[64] = \n{\n");
   for(i = 0; i < 64; i++)
     {
     if(!((i)%8))
@@ -227,8 +227,8 @@ int main(int argc, char ** argv)
   /* RGB 5/6 bit -> float */
 
 
-  fprintf(header, "extern float gavl_rgb_5_to_float[32];\n");
-  fprintf(source, "float gavl_rgb_5_to_float[32] = \n{\n");
+  fprintf(header, "extern const float gavl_rgb_5_to_float[32];\n");
+  fprintf(source, "const float gavl_rgb_5_to_float[32] = \n{\n");
   for(i = 0; i < 32; i++)
     {
     if(!((i)%8))
@@ -240,8 +240,8 @@ int main(int argc, char ** argv)
     }
   fprintf(source, "};\n\n");
 
-  fprintf(header, "extern float gavl_rgb_6_to_float[64];\n");
-  fprintf(source, "float gavl_rgb_6_to_float[64] = \n{\n");
+  fprintf(header, "extern const float gavl_rgb_6_to_float[64];\n");
+  fprintf(source, "const float gavl_rgb_6_to_float[64] = \n{\n");
   for(i = 0; i < 64; i++)
     {
     if(!((i)%8))
@@ -257,8 +257,8 @@ int main(int argc, char ** argv)
   fprintf(source, "/* RGB -> YUV conversions */\n");
 
     
-  fprintf(header, "extern int gavl_r_to_y[256];\n");
-  fprintf(source, "int gavl_r_to_y[256] = \n{\n");
+  fprintf(header, "extern const int gavl_r_to_y[256];\n");
+  fprintf(source, "const int gavl_r_to_y[256] = \n{\n");
   for(i = 0; i < 0x100; i++)
     {
     if(!((i)%8))
@@ -269,8 +269,8 @@ int main(int argc, char ** argv)
     }
   fprintf(source, "};\n\n");
 
-  fprintf(header, "extern int gavl_g_to_y[256];\n");
-  fprintf(source, "int gavl_g_to_y[256] = \n{\n");
+  fprintf(header, "extern const int gavl_g_to_y[256];\n");
+  fprintf(source, "const int gavl_g_to_y[256] = \n{\n");
   for(i = 0; i < 0x100; i++)
     {
     if(!((i)%8))
@@ -281,8 +281,8 @@ int main(int argc, char ** argv)
     }
   fprintf(source, "};\n\n");
 
-  fprintf(header, "extern int gavl_b_to_y[256];\n");
-  fprintf(source, "int gavl_b_to_y[256] = \n{\n");
+  fprintf(header, "extern const int gavl_b_to_y[256];\n");
+  fprintf(source, "const int gavl_b_to_y[256] = \n{\n");
   for(i = 0; i < 0x100; i++)
     {
     if(!((i)%8))
@@ -293,8 +293,8 @@ int main(int argc, char ** argv)
     }
   fprintf(source, "};\n\n");
 
-  fprintf(header, "extern int gavl_r_to_u[256];\n");
-  fprintf(source, "int gavl_r_to_u[256] = \n{\n");
+  fprintf(header, "extern const int gavl_r_to_u[256];\n");
+  fprintf(source, "const int gavl_r_to_u[256] = \n{\n");
   for(i = 0; i < 0x100; i++)
     {
     if(!((i)%8))
@@ -305,8 +305,8 @@ int main(int argc, char ** argv)
     }
   fprintf(source, "};\n\n");
   
-  fprintf(header, "extern int gavl_g_to_u[256];\n");
-  fprintf(source, "int gavl_g_to_u[256] = \n{\n");
+  fprintf(header, "extern const int gavl_g_to_u[256];\n");
+  fprintf(source, "const int gavl_g_to_u[256] = \n{\n");
   for(i = 0; i < 0x100; i++)
     {
     if(!((i)%8))
@@ -317,8 +317,8 @@ int main(int argc, char ** argv)
     }
   fprintf(source, "};\n\n");
 
-  fprintf(header, "extern int gavl_b_to_u[256];\n");
-  fprintf(source, "int gavl_b_to_u[256] = \n{\n");
+  fprintf(header, "extern const int gavl_b_to_u[256];\n");
+  fprintf(source, "const int gavl_b_to_u[256] = \n{\n");
   for(i = 0; i < 0x100; i++)
     {
     if(!((i)%8))
@@ -329,8 +329,8 @@ int main(int argc, char ** argv)
     }
   fprintf(source, "};\n\n");
     
-  fprintf(header, "extern int gavl_r_to_v[256];\n");
-  fprintf(source, "int gavl_r_to_v[256] = \n{\n");
+  fprintf(header, "extern const int gavl_r_to_v[256];\n");
+  fprintf(source, "const int gavl_r_to_v[256] = \n{\n");
   for(i = 0; i < 0x100; i++)
     {
     if(!((i)%8))
@@ -341,8 +341,8 @@ int main(int argc, char ** argv)
     }
   fprintf(source, "};\n\n");
 
-  fprintf(header, "extern int gavl_g_to_v[256];\n");
-  fprintf(source, "int gavl_g_to_v[256] = \n{\n");
+  fprintf(header, "extern const int gavl_g_to_v[256];\n");
+  fprintf(source, "const int gavl_g_to_v[256] = \n{\n");
   for(i = 0; i < 0x100; i++)
     {
     if(!((i)%8))
@@ -353,8 +353,8 @@ int main(int argc, char ** argv)
     }
   fprintf(source, "};\n\n");
 
-  fprintf(header, "extern int gavl_b_to_v[256];\n");
-  fprintf(source, "int gavl_b_to_v[256] = \n{\n");
+  fprintf(header, "extern const int gavl_b_to_v[256];\n");
+  fprintf(source, "const int gavl_b_to_v[256] = \n{\n");
   for(i = 0; i < 0x100; i++)
     {
     if(!((i)%8))
@@ -367,8 +367,8 @@ int main(int argc, char ** argv)
 
 
   
-  fprintf(header, "extern int gavl_r_to_yj[256];\n");
-  fprintf(source, "int gavl_r_to_yj[256] = \n{\n");
+  fprintf(header, "extern const int gavl_r_to_yj[256];\n");
+  fprintf(source, "const int gavl_r_to_yj[256] = \n{\n");
   for(i = 0; i < 0x100; i++)
     {
     if(!((i)%8))
@@ -379,8 +379,8 @@ int main(int argc, char ** argv)
     }
   fprintf(source, "};\n\n");
 
-  fprintf(header, "extern int gavl_g_to_yj[256];\n");
-  fprintf(source, "int gavl_g_to_yj[256] = \n{\n");
+  fprintf(header, "extern const int gavl_g_to_yj[256];\n");
+  fprintf(source, "const int gavl_g_to_yj[256] = \n{\n");
   for(i = 0; i < 0x100; i++)
     {
     if(!((i)%8))
@@ -391,8 +391,8 @@ int main(int argc, char ** argv)
     }
   fprintf(source, "};\n\n");
 
-  fprintf(header, "extern int gavl_b_to_yj[256];\n");
-  fprintf(source, "int gavl_b_to_yj[256] = \n{\n");
+  fprintf(header, "extern const int gavl_b_to_yj[256];\n");
+  fprintf(source, "const int gavl_b_to_yj[256] = \n{\n");
   for(i = 0; i < 0x100; i++)
     {
     if(!((i)%8))
@@ -403,8 +403,8 @@ int main(int argc, char ** argv)
     }
   fprintf(source, "};\n\n");
     
-  fprintf(header, "extern int gavl_r_to_uj[256];\n");
-  fprintf(source, "int gavl_r_to_uj[256] = \n{\n");
+  fprintf(header, "extern const int gavl_r_to_uj[256];\n");
+  fprintf(source, "const int gavl_r_to_uj[256] = \n{\n");
   for(i = 0; i < 0x100; i++)
     {
     if(!((i)%8))
@@ -415,8 +415,8 @@ int main(int argc, char ** argv)
     }
   fprintf(source, "};\n\n");
 
-  fprintf(header, "extern int gavl_g_to_uj[256];\n");
-  fprintf(source, "int gavl_g_to_uj[256] = \n{\n");
+  fprintf(header, "extern const int gavl_g_to_uj[256];\n");
+  fprintf(source, "const int gavl_g_to_uj[256] = \n{\n");
   for(i = 0; i < 0x100; i++)
     {
     if(!((i)%8))
@@ -427,8 +427,8 @@ int main(int argc, char ** argv)
     }
   fprintf(source, "};\n\n");
 
-  fprintf(header, "extern int gavl_b_to_uj[256];\n");
-  fprintf(source, "int gavl_b_to_uj[256] = \n{\n");
+  fprintf(header, "extern const int gavl_b_to_uj[256];\n");
+  fprintf(source, "const int gavl_b_to_uj[256] = \n{\n");
   for(i = 0; i < 0x100; i++)
     {
     if(!((i)%8))
@@ -439,8 +439,8 @@ int main(int argc, char ** argv)
     }
   fprintf(source, "};\n\n");
     
-  fprintf(header, "extern int gavl_r_to_vj[256];\n");
-  fprintf(source, "int gavl_r_to_vj[256] = \n{\n");
+  fprintf(header, "extern const int gavl_r_to_vj[256];\n");
+  fprintf(source, "const int gavl_r_to_vj[256] = \n{\n");
   for(i = 0; i < 0x100; i++)
     {
     if(!((i)%8))
@@ -451,8 +451,8 @@ int main(int argc, char ** argv)
     }
   fprintf(source, "};\n\n");
 
-  fprintf(header, "extern int gavl_g_to_vj[256];\n");
-  fprintf(source, "int gavl_g_to_vj[256] = \n{\n");
+  fprintf(header, "extern const int gavl_g_to_vj[256];\n");
+  fprintf(source, "const int gavl_g_to_vj[256] = \n{\n");
   for(i = 0; i < 0x100; i++)
     {
     if(!((i)%8))
@@ -463,8 +463,8 @@ int main(int argc, char ** argv)
     }
   fprintf(source, "};\n\n");
 
-  fprintf(header, "extern int gavl_b_to_vj[256];\n");
-  fprintf(source, "int gavl_b_to_vj[256] = \n{\n");
+  fprintf(header, "extern const int gavl_b_to_vj[256];\n");
+  fprintf(source, "const int gavl_b_to_vj[256] = \n{\n");
   for(i = 0; i < 0x100; i++)
     {
     if(!((i)%8))
@@ -481,8 +481,8 @@ int main(int argc, char ** argv)
   // YCbCr (8bit) -> R'G'B' (integer) according to CCIR 601
 
  
-  fprintf(header, "extern int gavl_y_to_rgb[256];\n");
-  fprintf(source, "int gavl_y_to_rgb[256] = \n{\n");
+  fprintf(header, "extern const int gavl_y_to_rgb[256];\n");
+  fprintf(source, "const int gavl_y_to_rgb[256] = \n{\n");
   for(i = 0; i < 0x100; i++)
     {
     if(!((i)%8))
@@ -494,8 +494,8 @@ int main(int argc, char ** argv)
     }
   fprintf(source, "};\n\n");
   
-  fprintf(header, "extern int gavl_v_to_r[256];\n");
-  fprintf(source, "int gavl_v_to_r[256] = \n{\n");
+  fprintf(header, "extern const int gavl_v_to_r[256];\n");
+  fprintf(source, "const int gavl_v_to_r[256] = \n{\n");
   for(i = 0; i < 0x100; i++)
     {
     if(!((i)%8))
@@ -506,8 +506,8 @@ int main(int argc, char ** argv)
     }
   fprintf(source, "};\n\n");
 
-  fprintf(header, "extern int gavl_u_to_g[256];\n");
-  fprintf(source, "int gavl_u_to_g[256] = \n{\n");
+  fprintf(header, "extern const int gavl_u_to_g[256];\n");
+  fprintf(source, "const int gavl_u_to_g[256] = \n{\n");
   for(i = 0; i < 0x100; i++)
     {
     if(!((i)%8))
@@ -518,8 +518,8 @@ int main(int argc, char ** argv)
     }
   fprintf(source, "};\n\n");
 
-  fprintf(header, "extern int gavl_v_to_g[256];\n");
-  fprintf(source, "int gavl_v_to_g[256] = \n{\n");
+  fprintf(header, "extern const int gavl_v_to_g[256];\n");
+  fprintf(source, "const int gavl_v_to_g[256] = \n{\n");
   for(i = 0; i < 0x100; i++)
     {
     if(!((i)%8))
@@ -530,8 +530,8 @@ int main(int argc, char ** argv)
     }
   fprintf(source, "};\n\n");
 
-  fprintf(header, "extern int gavl_u_to_b[256];\n");
-  fprintf(source, "int gavl_u_to_b[256] = \n{\n");
+  fprintf(header, "extern const int gavl_u_to_b[256];\n");
+  fprintf(source, "const int gavl_u_to_b[256] = \n{\n");
   for(i = 0; i < 0x100; i++)
     {
     if(!((i)%8))
@@ -546,8 +546,8 @@ int main(int argc, char ** argv)
   /* JPEG Quantization */
 
   
-  fprintf(header, "extern int gavl_yj_to_rgb[256];\n");
-  fprintf(source, "int gavl_yj_to_rgb[256] = \n{\n");
+  fprintf(header, "extern const int gavl_yj_to_rgb[256];\n");
+  fprintf(source, "const int gavl_yj_to_rgb[256] = \n{\n");
   for(i = 0; i < 0x100; i++)
     {
     if(!((i)%8))
@@ -558,8 +558,8 @@ int main(int argc, char ** argv)
     }
   fprintf(source, "};\n\n");
     
-  fprintf(header, "extern int gavl_vj_to_r[256];\n");
-  fprintf(source, "int gavl_vj_to_r[256] = \n{\n");
+  fprintf(header, "extern const int gavl_vj_to_r[256];\n");
+  fprintf(source, "const int gavl_vj_to_r[256] = \n{\n");
   for(i = 0; i < 0x100; i++)
     {
     if(!((i)%8))
@@ -570,8 +570,8 @@ int main(int argc, char ** argv)
     }
   fprintf(source, "};\n\n");
 
-  fprintf(header, "extern int gavl_uj_to_g[256];\n");
-  fprintf(source, "int gavl_uj_to_g[256] = \n{\n");
+  fprintf(header, "extern const int gavl_uj_to_g[256];\n");
+  fprintf(source, "const int gavl_uj_to_g[256] = \n{\n");
   for(i = 0; i < 0x100; i++)
     {
     if(!((i)%8))
@@ -582,8 +582,8 @@ int main(int argc, char ** argv)
     }
   fprintf(source, "};\n\n");
 
-  fprintf(header, "extern int gavl_vj_to_g[256];\n");
-  fprintf(source, "int gavl_vj_to_g[256] = \n{\n");
+  fprintf(header, "extern const int gavl_vj_to_g[256];\n");
+  fprintf(source, "const int gavl_vj_to_g[256] = \n{\n");
   for(i = 0; i < 0x100; i++)
     {
     if(!((i)%8))
@@ -594,8 +594,8 @@ int main(int argc, char ** argv)
     }
   fprintf(source, "};\n\n");
 
-  fprintf(header, "extern int gavl_uj_to_b[256];\n");
-  fprintf(source, "int gavl_uj_to_b[256] = \n{\n");
+  fprintf(header, "extern const int gavl_uj_to_b[256];\n");
+  fprintf(source, "const int gavl_uj_to_b[256] = \n{\n");
   for(i = 0; i < 0x100; i++)
     {
     if(!((i)%8))
@@ -610,8 +610,8 @@ int main(int argc, char ** argv)
   // YCbCr (8bit) -> R'G'B' (float) according to CCIR 601
 
     
-  fprintf(header, "extern float gavl_y_to_rgb_float[256];\n");
-  fprintf(source, "float gavl_y_to_rgb_float[256] = \n{\n");
+  fprintf(header, "extern const float gavl_y_to_rgb_float[256];\n");
+  fprintf(source, "const float gavl_y_to_rgb_float[256] = \n{\n");
   for(i = 0; i < 0x100; i++)
     {
     if(!((i)%8))
@@ -623,8 +623,8 @@ int main(int argc, char ** argv)
     }
   fprintf(source, "};\n\n");
   
-  fprintf(header, "extern float gavl_v_to_r_float[256];\n");
-  fprintf(source, "float gavl_v_to_r_float[256] = \n{\n");
+  fprintf(header, "extern const float gavl_v_to_r_float[256];\n");
+  fprintf(source, "const float gavl_v_to_r_float[256] = \n{\n");
   for(i = 0; i < 0x100; i++)
     {
     if(!((i)%8))
@@ -635,8 +635,8 @@ int main(int argc, char ** argv)
     }
   fprintf(source, "};\n\n");
 
-  fprintf(header, "extern float gavl_u_to_g_float[256];\n");
-  fprintf(source, "float gavl_u_to_g_float[256] = \n{\n");
+  fprintf(header, "extern const float gavl_u_to_g_float[256];\n");
+  fprintf(source, "const float gavl_u_to_g_float[256] = \n{\n");
   for(i = 0; i < 0x100; i++)
     {
     if(!((i)%8))
@@ -647,8 +647,8 @@ int main(int argc, char ** argv)
     }
   fprintf(source, "};\n\n");
 
-  fprintf(header, "extern float gavl_v_to_g_float[256];\n");
-  fprintf(source, "float gavl_v_to_g_float[256] = \n{\n");
+  fprintf(header, "extern const float gavl_v_to_g_float[256];\n");
+  fprintf(source, "const float gavl_v_to_g_float[256] = \n{\n");
   for(i = 0; i < 0x100; i++)
     {
     if(!((i)%8))
@@ -659,8 +659,8 @@ int main(int argc, char ** argv)
     }
   fprintf(source, "};\n\n");
 
-  fprintf(header, "extern float gavl_u_to_b_float[256];\n");
-  fprintf(source, "float gavl_u_to_b_float[256] = \n{\n");
+  fprintf(header, "extern const float gavl_u_to_b_float[256];\n");
+  fprintf(source, "const float gavl_u_to_b_float[256] = \n{\n");
   for(i = 0; i < 0x100; i++)
     {
     if(!((i)%8))
@@ -674,8 +674,8 @@ int main(int argc, char ** argv)
   /* JPEG Quantization */
 
   
-  fprintf(header, "extern float gavl_yj_to_rgb_float[256];\n");
-  fprintf(source, "float gavl_yj_to_rgb_float[256] = \n{\n");
+  fprintf(header, "extern const float gavl_yj_to_rgb_float[256];\n");
+  fprintf(source, "const float gavl_yj_to_rgb_float[256] = \n{\n");
   for(i = 0; i < 0x100; i++)
     {
     if(!((i)%8))
@@ -686,8 +686,8 @@ int main(int argc, char ** argv)
     }
   fprintf(source, "};\n\n");
     
-  fprintf(header, "extern float gavl_vj_to_r_float[256];\n");
-  fprintf(source, "float gavl_vj_to_r_float[256] = \n{\n");
+  fprintf(header, "extern const float gavl_vj_to_r_float[256];\n");
+  fprintf(source, "const float gavl_vj_to_r_float[256] = \n{\n");
   for(i = 0; i < 0x100; i++)
     {
     if(!((i)%8))
@@ -698,8 +698,8 @@ int main(int argc, char ** argv)
     }
   fprintf(source, "};\n\n");
 
-  fprintf(header, "extern float gavl_uj_to_g_float[256];\n");
-  fprintf(source, "float gavl_uj_to_g_float[256] = \n{\n");
+  fprintf(header, "extern const float gavl_uj_to_g_float[256];\n");
+  fprintf(source, "const float gavl_uj_to_g_float[256] = \n{\n");
   for(i = 0; i < 0x100; i++)
     {
     if(!((i)%8))
@@ -710,8 +710,8 @@ int main(int argc, char ** argv)
     }
   fprintf(source, "};\n\n");
 
-  fprintf(header, "extern float gavl_vj_to_g_float[256];\n");
-  fprintf(source, "float gavl_vj_to_g_float[256] = \n{\n");
+  fprintf(header, "extern const float gavl_vj_to_g_float[256];\n");
+  fprintf(source, "const float gavl_vj_to_g_float[256] = \n{\n");
   for(i = 0; i < 0x100; i++)
     {
     if(!((i)%8))
@@ -722,8 +722,8 @@ int main(int argc, char ** argv)
     }
   fprintf(source, "};\n\n");
 
-  fprintf(header, "extern float gavl_uj_to_b_float[256];\n");
-  fprintf(source, "float gavl_uj_to_b_float[256] = \n{\n");
+  fprintf(header, "extern const float gavl_uj_to_b_float[256];\n");
+  fprintf(source, "const float gavl_uj_to_b_float[256] = \n{\n");
   for(i = 0; i < 0x100; i++)
     {
     if(!((i)%8))
