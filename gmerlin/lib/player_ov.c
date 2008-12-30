6c4112d4fadb755123859d8c0022cf09a5b57b8a
gmerlin <gmerlin@214d8ac5-8513-4f59-80f8-2f39b674a24a>
2008-12-30 23:03:36 +0000

    * Reversed change for A/V sync under heavy load (messed up seeking)
    git-svn-id: svn://svn.code.sf.net/p/gmerlin/code/trunk@1854 214d8ac5-8513-4f59-80f8-2f39b674a24a

--- gmerlin/lib/player_ov.c
+++ gmerlin/lib/player_ov.c
@@ -43,7 +43,7 @@ struct bg_player_ov_context_s
   bg_ov_callbacks_t callbacks;
   gavl_video_frame_t * frame;
   gavl_time_t frame_time;
-  gavl_time_t last_time; /* Last player time */
+  //  gavl_time_t last_time; /* Last player time */
   
   gavl_video_frame_t * still_frame;
   pthread_mutex_t     still_mutex;
@@ -437,7 +437,6 @@ int bg_player_ov_init(bg_player_ov_context_t * ctx)
   
   ctx->next_subtitle    = (gavl_overlay_t*)0;
   ctx->has_subtitle = 0;
-  ctx->last_time = GAVL_TIME_UNDEFINED;
 
   bg_plugin_lock(ctx->plugin_handle);
   result = ctx->plugin->open(ctx->priv,
@@ -536,7 +535,6 @@ void bg_player_ov_reset(bg_player_t * player)
   {
   bg_player_ov_context_t * ctx;
   ctx = player->ov_context;
-  ctx->last_time = GAVL_TIME_UNDEFINED;
   if(ctx->has_subtitle)
     {
     ctx->plugin->set_overlay(ctx->priv, ctx->subtitle_id, (gavl_overlay_t*)0);
@@ -736,13 +734,8 @@ void * bg_player_ov_thread(void * data)
     /* Check Timing */
     bg_player_time_get(ctx->player, 1, &current_time);
 
-    if((ctx->last_time == GAVL_TIME_UNDEFINED) ||
-       (ctx->last_time != current_time))
-      diff_time =  ctx->frame_time - current_time;
-    else
-      diff_time =  0;
+    diff_time =  ctx->frame_time - current_time;
     
-    ctx->last_time = current_time;
     
 #ifdef DUMP_TIMESTAMPS
     bg_debug("C: %"PRId64", F: %"PRId64", D: %"PRId64"\n",
