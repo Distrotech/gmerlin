f7f1f3468ee6b321884281ba53f587577281b6ed
gmerlin <gmerlin@214d8ac5-8513-4f59-80f8-2f39b674a24a>
2009-07-03 21:27:13 +0000

    * make dist fix
    git-svn-id: svn://svn.code.sf.net/p/gmerlin/code/trunk@2096 214d8ac5-8513-4f59-80f8-2f39b674a24a

---
+++ gmerlerra/include/types.h
@@ -0,0 +1,20 @@
+#ifndef BG_NLE_TYPES_H
+#define BG_NLE_TYPES_H
+
+typedef int32_t bg_nle_id_t;
+
+typedef enum
+  {
+    BG_NLE_TRACK_NONE = 0,
+    BG_NLE_TRACK_AUDIO,
+    BG_NLE_TRACK_VIDEO,
+  } bg_nle_track_type_t;
+
+/* WARNING: Changing these alters the file format */
+#define BG_NLE_TRACK_SELECTED (1<<0)
+#define BG_NLE_TRACK_EXPANDED (1<<1)
+#define BG_NLE_TRACK_PLAYBACK (1<<2)
+
+typedef struct bg_nle_project_s bg_nle_project_t;
+
+#endif
