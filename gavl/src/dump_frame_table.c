bbf900441c0c3176bbe0d1a21f277c8f83e99980
gmerlin <gmerlin@214d8ac5-8513-4f59-80f8-2f39b674a24a>
2009-08-08 14:26:35 +0000

    * dump_frame_table
    git-svn-id: svn://svn.code.sf.net/p/gmerlin/code/trunk@2168 214d8ac5-8513-4f59-80f8-2f39b674a24a

---
+++ gavl/src/dump_frame_table.c
@@ -0,0 +1,31 @@
+/*****************************************************************
+ * gavl - a general purpose audio/video processing library
+ *
+ * Copyright (c) 2001 - 2008 Members of the Gmerlin project
+ * gmerlin-general@lists.sourceforge.net
+ * http://gmerlin.sourceforge.net
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * *****************************************************************/
+
+#include <gavl/gavl.h>
+
+int main(int argc, char ** argv)
+  {
+  gavl_frame_table_t * tab;
+  tab = gavl_frame_table_load(argv[1]);
+  gavl_frame_table_dump(tab);
+  gavl_frame_table_destroy(tab);
+  return 0;
+  }
