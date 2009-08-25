d1a6da71a78c560a0eb7d5e221d02163bb286b01
gmerlin <gmerlin@214d8ac5-8513-4f59-80f8-2f39b674a24a>
2009-08-25 08:47:50 +0000

    * Workaround fedora compilation problem
    git-svn-id: svn://svn.code.sf.net/p/gmerlin/code/trunk@2173 214d8ac5-8513-4f59-80f8-2f39b674a24a

--- gmerlin_avdecoder/lib/dvb_channels.c
+++ gmerlin_avdecoder/lib/dvb_channels.c
@@ -19,15 +19,16 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  * *****************************************************************/
 
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+
 #include <linux/dvb/frontend.h>
 
 #include <avdec_private.h>
 #include <dvb_channels.h>
 
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
-#include <errno.h>
 
 #define LOG_DOMAIN "dvb_channels"
 
