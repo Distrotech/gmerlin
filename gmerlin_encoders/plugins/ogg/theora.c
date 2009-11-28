37ddd975b610cf7f5b8e2486470eedf9042dacd6
gmerlin <gmerlin@214d8ac5-8513-4f59-80f8-2f39b674a24a>
2009-11-28 00:08:44 +0000

    * Update for new theora API
    git-svn-id: svn://svn.code.sf.net/p/gmerlin/code/trunk@2261 214d8ac5-8513-4f59-80f8-2f39b674a24a

--- gmerlin_encoders/plugins/ogg/theora.c
+++ gmerlin_encoders/plugins/ogg/theora.c
@@ -31,8 +31,8 @@
 #include <gmerlin/log.h>
 #define LOG_DOMAIN "oggtheora"
 
+#include <theora/theoraenc.h>
 
-#include <theora/theora.h>
 #include "ogg_common.h"
 
 typedef struct
@@ -41,9 +41,9 @@ typedef struct
     
   ogg_stream_state os;
   
-  theora_info    ti;
-  theora_comment tc;
-  theora_state   ts;
+  th_info        ti;
+  th_comment     tc;
+  th_enc_ctx   * ts;
   
   long serialno;
   bg_ogg_encoder_t * output;
@@ -51,6 +51,11 @@ typedef struct
   gavl_video_format_t * format;
   int have_packet;
   int cbr;
+  int max_keyframe_interval;
+  
+  th_ycbcr_buffer buf;
+
+  float speed;
   } theora_t;
 
 static void * create_theora(bg_ogg_encoder_t * output, long serialno)
@@ -59,8 +64,8 @@ static void * create_theora(bg_ogg_encoder_t * output, long serialno)
   ret = calloc(1, sizeof(*ret));
   ret->serialno = serialno;
   ret->output = output;
-  theora_info_init(&ret->ti);
-
+  th_info_init(&ret->ti);
+  
   return ret;
   }
 
@@ -93,56 +98,22 @@ static const bg_parameter_info_t parameters[] =
 0:  worst (smallest output file)"),
     },
     {
-      .name =      "keyframe_auto_p",
-      .long_name = TRS("Automatic keyframe insertion"),
-      .type =      BG_PARAMETER_CHECKBUTTON,
-      .val_default = { .val_i = 0 },
-    },
-    {
-      .name =      "keyframe_frequency",
-      .long_name = TRS("Keyframe frequency"),
+      .name =      "max_keyframe_interval",
+      .long_name = TRS("Maximum keyframe interval"),
       .type =      BG_PARAMETER_INT,
-      .val_default = { .val_i = 64 },
+      .val_min =     { .val_i = 1    },
+      .val_max =     { .val_i = 1000 },
+      .val_default = { .val_i = 64   },
     },
     {
-      .name =      "keyframe_frequency_force",
-      .long_name = TRS("Keyframe frequency force"),
-      .type =      BG_PARAMETER_INT,
-      .val_default = { .val_i = 64 },
-    },
-    {
-      .name =      "keyframe_auto_threshold",
-      .long_name = TRS("Keyframe auto threshold"),
-      .type =      BG_PARAMETER_INT,
-      .val_default = { .val_i = 80 },
-    },
-    {
-      .name =      "keyframe_mindistance",
-      .long_name = TRS("Keyframe minimum distance"),
-      .type =      BG_PARAMETER_INT,
-      .val_default = { .val_i = 8 },
-    },
-    {
-      .name =      "quick_encode",
-      .long_name = TRS("Quick encode"),
-      .type =      BG_PARAMETER_CHECKBUTTON,
-      .val_default = { .val_i = 0 },
-    },
-    {
-      .name =      "sharpness",
-      .long_name = TRS("Sharpness"),
-      .type =      BG_PARAMETER_SLIDER_INT,
-      .val_default = { .val_i = 0 },
-      .val_min =     { .val_i = 0 },
-      .val_max =     { .val_i = 2 },
-    },
-    {
-      .name =      "noise_sensitivity",
-      .long_name = TRS("Noise Sensitivity"),
-      .type =      BG_PARAMETER_SLIDER_INT,
-      .val_default = { .val_i = 0 },
-      .val_min =     { .val_i = 0 },
-      .val_max =     { .val_i = 7 },
+      .name =      "speed",
+      .long_name = TRS("Encoding speed"),
+      .type =      BG_PARAMETER_SLIDER_FLOAT,
+      .val_min =     { .val_f = 0.0 },
+      .val_max =     { .val_f = 1.0 },
+      .val_default = { .val_f = 0.0 },
+      .num_digits  = 2,
+      .help_string = TRS("Higher speed levels favor quicker encoding over better quality per bit. Depending on the encoding mode, and the internal algorithms used, quality may actually improve, but in this case bitrate will also likely increase. In any case, overall rate/distortion performance will probably decrease."),
     },
     { /* End of parameters */ }
   };
@@ -159,90 +130,54 @@ static void set_parameter_theora(void * data, const char * name,
   theora = (theora_t*)data;
   
   if(!name)
-    {
     return;
-    }
-  if(!strcmp(name, "target_bitrate"))
-    {
+  else if(!strcmp(name, "target_bitrate"))
     theora->ti.target_bitrate = v->val_i * 1000;
-    }
   else if(!strcmp(name, "quality"))
-    {
     theora->ti.quality = v->val_i;
-    }
-  else if(!strcmp(name, "quick_encode"))
-    {
-    theora->ti.quick_p = v->val_i;
-    }
-  else if(!strcmp(name, "keyframe_auto_p"))
-    {
-    theora->ti.keyframe_auto_p = v->val_i;
-    }
   else if(!strcmp(name, "cbr"))
-    {
     theora->cbr = v->val_i;
-    }
-  else if(!strcmp(name, "sharpness"))
-    {
-    theora->ti.sharpness = v->val_i;
-    }
-  else if(!strcmp(name, "noise_sensitivity"))
-    {
-    theora->ti.noise_sensitivity = v->val_i;
-    }
-  else if(!strcmp(name, "keyframe_frequency"))
-    {
-    theora->ti.keyframe_frequency = v->val_i;
-    }
-  else if(!strcmp(name, "keyframe_frequency_force"))
-    {
-    theora->ti.keyframe_frequency_force = v->val_i;
-    }
-  else if(!strcmp(name, "keyframe_auto_threshold"))
-    {
-    theora->ti.keyframe_auto_threshold = v->val_i;
-    }
-  else if(!strcmp(name, "keyframe_mindistance"))
-    {
-    theora->ti.keyframe_mindistance = v->val_i;
-    }
+  else if(!strcmp(name, "max_keyframe_interval"))
+    theora->max_keyframe_interval = v->val_i;
+  else if(!strcmp(name, "speed"))
+    theora->speed = v->val_f;
   
   }
 
 
-static void build_comment(theora_comment * vc, bg_metadata_t * metadata)
+static void build_comment(th_comment * vc, bg_metadata_t * metadata)
   {
   char * tmp_string;
   
-  theora_comment_init(vc);
+  th_comment_init(vc);
   
   if(metadata->artist)
-    theora_comment_add_tag(vc, "ARTIST", metadata->artist);
+    th_comment_add_tag(vc, "ARTIST", metadata->artist);
   
   if(metadata->title)
-    theora_comment_add_tag(vc, "TITLE", metadata->title);
+    th_comment_add_tag(vc, "TITLE", metadata->title);
 
   if(metadata->album)
-    theora_comment_add_tag(vc, "ALBUM", metadata->album);
+    th_comment_add_tag(vc, "ALBUM", metadata->album);
     
   if(metadata->genre)
-    theora_comment_add_tag(vc, "GENRE", metadata->genre);
+    th_comment_add_tag(vc, "GENRE", metadata->genre);
 
   if(metadata->date)
-    theora_comment_add_tag(vc, "DATE", metadata->date);
+    th_comment_add_tag(vc, "DATE", metadata->date);
   
   if(metadata->copyright)
-    theora_comment_add_tag(vc, "COPYRIGHT", metadata->copyright);
+    th_comment_add_tag(vc, "COPYRIGHT", metadata->copyright);
 
   if(metadata->track)
     {
     tmp_string = bg_sprintf("%d", metadata->track);
-    theora_comment_add_tag(vc, "TRACKNUMBER", tmp_string);
+    th_comment_add_tag(vc, "TRACKNUMBER", tmp_string);
     free(tmp_string);
     }
 
   if(metadata->comment)
-    theora_comment_add(vc, metadata->comment);
+    th_comment_add(vc, metadata->comment);
   }
 
 static const gavl_pixelformat_t supported_pixelformats[] =
@@ -255,17 +190,21 @@ static const gavl_pixelformat_t supported_pixelformats[] =
 
 static int init_theora(void * data, gavl_video_format_t * format, bg_metadata_t * metadata)
   {
+  int sub_h, sub_v;
+  int arg_i1, arg_i2;
+  
   ogg_packet op;
-
+  int header_packets;
+  
   theora_t * theora = (theora_t *)data;
 
   theora->format = format;
   
   /* Set video format */
-  theora->ti.width  = ((format->image_width  + 15)/16*16);
-  theora->ti.height = ((format->image_height + 15)/16*16);
-  theora->ti.frame_width = format->image_width;
-  theora->ti.frame_height = format->image_height;
+  theora->ti.frame_width  = ((format->image_width  + 15)/16*16);
+  theora->ti.frame_height = ((format->image_height + 15)/16*16);
+  theora->ti.pic_width = format->image_width;
+  theora->ti.pic_height = format->image_height;
   
   theora->ti.fps_numerator      = format->timescale;
   theora->ti.fps_denominator    = format->frame_duration;
@@ -274,23 +213,21 @@ static int init_theora(void * data, gavl_video_format_t * format, bg_metadata_t 
 
   format->interlace_mode = GAVL_INTERLACE_NONE;
   format->framerate_mode = GAVL_FRAMERATE_CONSTANT;
-  format->frame_width  = theora->ti.width;
-  format->frame_height = theora->ti.height;
-
-  /* TODO: Configure these */
-
+  format->frame_width  = theora->ti.frame_width;
+  format->frame_height = theora->ti.frame_height;
+  
   if(theora->cbr)
     theora->ti.quality = 0;
   else
     theora->ti.target_bitrate = 0;
+
+  /* Granule shift */
+  theora->ti.keyframe_granule_shift = 0;
+
+  while(1 << theora->ti.keyframe_granule_shift < theora->max_keyframe_interval)
+    theora->ti.keyframe_granule_shift++;
   
-  theora->ti.colorspace=OC_CS_UNSPECIFIED;
-  theora->ti.dropframes_p=0;
-  theora->ti.keyframe_frequency=64;
-  theora->ti.keyframe_frequency_force=64;
-  theora->ti.keyframe_data_target_bitrate=theora->ti.keyframe_data_target_bitrate*1.5;
-  theora->ti.keyframe_auto_threshold=80;
-  theora->ti.keyframe_mindistance=8;
+  theora->ti.colorspace=TH_CS_UNSPECIFIED;
 
   format->pixelformat =
     gavl_pixelformat_get_best(format->pixelformat,
@@ -299,13 +236,13 @@ static int init_theora(void * data, gavl_video_format_t * format, bg_metadata_t 
   switch(format->pixelformat)
     {
     case GAVL_YUV_420_P:
-      theora->ti.pixelformat = OC_PF_420;
+      theora->ti.pixel_fmt = TH_PF_420;
       break;
     case GAVL_YUV_422_P:
-      theora->ti.pixelformat = OC_PF_422;
+      theora->ti.pixel_fmt = TH_PF_422;
       break;
     case GAVL_YUV_444_P:
-      theora->ti.pixelformat = OC_PF_444;
+      theora->ti.pixel_fmt = TH_PF_444;
       break;
     default:
       return 0;
@@ -314,37 +251,74 @@ static int init_theora(void * data, gavl_video_format_t * format, bg_metadata_t 
   ogg_stream_init(&theora->os, theora->serialno);
 
   /* Initialize encoder */
-  if(theora_encode_init(&theora->ts, &theora->ti))
+  if(!(theora->ts = th_encode_alloc(&theora->ti)))
     {
-    bg_log(BG_LOG_ERROR, LOG_DOMAIN,  "theora_encode_init failed");
+    bg_log(BG_LOG_ERROR, LOG_DOMAIN,  "th_encode_alloc failed");
     return 0;
     }
   /* Build comment (comments are UTF-8, good for us :-) */
 
   build_comment(&theora->tc, metadata);
 
-  /* Encode initial packets */
-  if(theora_encode_header(&theora->ts, &op))
+  /* Call encode CTLs */
+  
+  // Keyframe frequency
+
+  th_encode_ctl(theora->ts,
+                TH_ENCCTL_SET_KEYFRAME_FREQUENCY_FORCE,
+                &theora->max_keyframe_interval, sizeof(arg_i1));
+  
+  // Maximum speed
+
+  if(th_encode_ctl(theora->ts,
+                   TH_ENCCTL_GET_SPLEVEL_MAX,
+                   &arg_i1, sizeof(arg_i1)) != TH_EIMPL)
     {
-    bg_log(BG_LOG_ERROR, LOG_DOMAIN,  "theora_encode_header failed");
+    arg_i2 = (int)((float)arg_i1 * theora->speed + 0.5);
+
+    if(arg_i2 > arg_i1)
+      arg_i2 = arg_i1;
+    
+    th_encode_ctl(theora->ts, TH_ENCCTL_SET_SPLEVEL,
+                  &arg_i2, sizeof(arg_i2));
+    }
+  
+  /* Encode initial packets */
+
+  header_packets = 0;
+  
+  while(th_encode_flushheader(theora->ts, &theora->tc, &op) > 0)
+    {
+    ogg_stream_packetin(&theora->os,&op);
+
+    if(!header_packets)
+      {
+      /* And stream them out */
+      if(!bg_ogg_flush_page(&theora->os, theora->output, 1))
+        {
+        bg_log(BG_LOG_ERROR, LOG_DOMAIN,  "Got no Theora ID page");
+        return 0;
+        }
+      }
+    header_packets++;
+    }
+  
+  if(header_packets < 3)
+    {
+    bg_log(BG_LOG_ERROR, LOG_DOMAIN,  "Got %d header packets instead of 3", header_packets);
     return 0;
     }
-  else
-    {
-    /* And stream them out */
-    ogg_stream_packetin(&theora->os,&op);
-    if(!bg_ogg_flush_page(&theora->os, theora->output, 1))
-      {
-      bg_log(BG_LOG_ERROR, LOG_DOMAIN,  "Got no Theora ID page");
-      return 0;
-      }
-    }
-  theora_encode_comment(&theora->tc, &op);
-  ogg_stream_packetin(&theora->os,&op);
-  
-  theora_encode_tables(&theora->ts, &op);
-  ogg_stream_packetin(&theora->os,&op);
 
+  /* Initialize buffer */
+  
+  gavl_pixelformat_chroma_sub(theora->format->pixelformat, &sub_h, &sub_v);
+  theora->buf[0].width  = theora->format->frame_width;
+  theora->buf[0].height = theora->format->frame_height;
+  theora->buf[1].width  = theora->format->frame_width  / sub_h;
+  theora->buf[1].height = theora->format->frame_height / sub_v;
+  theora->buf[2].width  = theora->format->frame_width  / sub_h;
+  theora->buf[2].height = theora->format->frame_height / sub_v;
+  
   return 1;
   }
 
@@ -361,21 +335,18 @@ static int flush_header_pages_theora(void*data)
 static int write_video_frame_theora(void * data, gavl_video_frame_t * frame)
   {
   theora_t * theora;
-  int sub_h, sub_v;
   int result;
-  yuv_buffer yuv;
+  int i;
   ogg_packet op;
   
   theora = (theora_t*)data;
-
+  
   if(theora->have_packet)
     {
-    if(!theora_encode_packetout(&theora->ts, 0, &op))
+    if(!th_encode_packetout(theora->ts, 0, &op))
       {
       bg_log(BG_LOG_ERROR, LOG_DOMAIN,
              "Theora encoder produced no packet");
-      fprintf(stderr, "Packetout failed\n");
-      
       return 0;
       }
     
@@ -388,21 +359,14 @@ static int write_video_frame_theora(void * data, gavl_video_frame_t * frame)
       }
     theora->have_packet = 0;
     }
-  
-  gavl_pixelformat_chroma_sub(theora->format->pixelformat, &sub_h, &sub_v);
-  
-  yuv.y_width  = theora->format->frame_width;
-  yuv.y_height = theora->format->frame_height;
-  yuv.y_stride = frame->strides[0];
 
-  yuv.uv_width  = theora->format->frame_width / sub_h;
-  yuv.uv_height = theora->format->frame_height / sub_v;
-  yuv.uv_stride = frame->strides[1];
-
-  yuv.y = frame->planes[0];
-  yuv.u = frame->planes[1];
-  yuv.v = frame->planes[2];
-  result = theora_encode_YUVin(&theora->ts, &yuv);
+  for(i = 0; i < 3; i++)
+    {
+    theora->buf[i].stride = frame->strides[i];
+    theora->buf[i].data   = frame->planes[i];
+    }
+  
+  result = th_encode_ycbcr_in(theora->ts, theora->buf);
   theora->have_packet = 1;
   return 1;
   }
@@ -416,7 +380,7 @@ static int close_theora(void * data)
 
   if(theora->have_packet)
     {
-    if(!theora_encode_packetout(&theora->ts, 1, &op))
+    if(!th_encode_packetout(theora->ts, 1, &op))
       {
       bg_log(BG_LOG_ERROR, LOG_DOMAIN, "Theora encoder produced no packet");
       ret = 0;
@@ -435,9 +399,9 @@ static int close_theora(void * data)
     }
   
   ogg_stream_clear(&theora->os);
-  theora_comment_clear(&theora->tc);
-  theora_info_clear(&theora->ti);
-  theora_clear(&theora->ts);
+  th_comment_clear(&theora->tc);
+  th_info_clear(&theora->ti);
+  th_encode_free(theora->ts);
   free(theora);
   return ret;
   }
