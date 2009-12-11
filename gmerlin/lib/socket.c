02af3ccb68626c57b605537be1bf40d9b90afcc6
gmerlin <gmerlin@214d8ac5-8513-4f59-80f8-2f39b674a24a>
2009-12-11 22:48:17 +0000

    * Fix
    git-svn-id: svn://svn.code.sf.net/p/gmerlin/code/trunk@2295 214d8ac5-8513-4f59-80f8-2f39b674a24a

--- gmerlin/lib/socket.c
+++ gmerlin/lib/socket.c
@@ -279,7 +279,7 @@ int bg_socket_connect_inet(bg_host_address_t * a, int milliseconds)
       }
     else
       {
-      bg_log(BG_LOG_ERROR, LOG_DOMAIN, "Connecting failed: %s", strerror(errno));
+      bg_log(BG_LOG_ERROR, LOG_DOMAIN, "Connecting failed: %s", strerror(err));
       return -1;
       }
     }
