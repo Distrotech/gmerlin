828bb15ac3ac0b44fb0d3ff53b97b984c8642929
gmerlin <gmerlin@214d8ac5-8513-4f59-80f8-2f39b674a24a>
2011-01-07 15:30:16 +0000

    * Copyright update
    git-svn-id: svn://svn.code.sf.net/p/gmerlin/code/trunk@2761 214d8ac5-8513-4f59-80f8-2f39b674a24a

--- gavl/update_copyright.sh
+++ gavl/update_copyright.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 NEW_YEAR=`date +%Y`
-OLD_YEAR=`echo "$YEAR - 1" | bc`
+OLD_YEAR=`echo "$NEW_YEAR - 1" | bc`
 OLD_YEARS="2001 - $OLD_YEAR"
 NEW_YEARS="2001 - $NEW_YEAR"
+echo "Replacing \"$OLD_YEARS\" with \"$NEW_YEARS\""
 find . -name '*.[ch]' -exec sed -i "s/$OLD_YEARS/$NEW_YEARS/g" {} \;
