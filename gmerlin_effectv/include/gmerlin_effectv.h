3b6f39c3a882ae0ad0f80478cd278768272c51ea
gmerlin <gmerlin@214d8ac5-8513-4f59-80f8-2f39b674a24a>
2012-06-29 23:36:13 +0000

    * Copyright update
    git-svn-id: svn://svn.code.sf.net/p/gmerlin/code/trunk@3159 214d8ac5-8513-4f59-80f8-2f39b674a24a

--- gmerlin_effectv/include/gmerlin_effectv.h
+++ gmerlin_effectv/include/gmerlin_effectv.h
@@ -1,7 +1,7 @@
 /*****************************************************************
  * gmerlin_effectv - EffecTV plugins for gmerlin
  *
- * Copyright (c) 2001 - 2011 Members of the Gmerlin project
+ * Copyright (c) 2001 - 2012 Members of the Gmerlin project
  * gmerlin-general@lists.sourceforge.net
  * http://gmerlin.sourceforge.net
  *
