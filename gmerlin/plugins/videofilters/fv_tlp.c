2eddfd1b925d46d2fce4ee77e9b7e476f4029c43
gmerlin <gmerlin@214d8ac5-8513-4f59-80f8-2f39b674a24a>
2012-08-22 22:29:01 +0000

    * Updated tlp
    git-svn-id: svn://svn.code.sf.net/p/gmerlin/code/trunk@3253 214d8ac5-8513-4f59-80f8-2f39b674a24a

--- gmerlin/plugins/videofilters/fv_tlp.c
+++ gmerlin/plugins/videofilters/fv_tlp.c
@@ -37,17 +37,19 @@ typedef struct tlp_priv_s
   {
   float factor;
   
-  bg_read_video_func_t read_func;
-  void * read_data;
-  int read_stream;
-  
   gavl_video_format_t format;
   
-  gavl_video_frame_t * frame_1;
-  gavl_video_frame_t * frame_2;
+  gavl_video_frame_t * frames[2];
+
+  gavl_video_frame_t * this_frame;
+  gavl_video_frame_t * last_frame;
   
   gavl_dsp_context_t * ctx;
   int init;
+
+  gavl_video_source_t * in_src;
+  gavl_video_source_t * out_src;
+
   } tlp_priv_t;
 
 static void * create_tlp()
@@ -62,12 +64,16 @@ static void destroy_tlp(void * priv)
   {
   tlp_priv_t * vp;
   vp = priv;
-  if(vp->frame_1)
-    gavl_video_frame_destroy(vp->frame_1);
-  if(vp->frame_2)
-    gavl_video_frame_destroy(vp->frame_2);
+  if(vp->frames[0])
+    gavl_video_frame_destroy(vp->frames[0]);
+  if(vp->frames[1])
+    gavl_video_frame_destroy(vp->frames[1]);
   if(vp->ctx)
     gavl_dsp_context_destroy(vp->ctx);
+
+  if(vp->out_src)
+    gavl_video_source_destroy(vp->out_src);
+
   free(vp);
   }
 
@@ -116,49 +122,6 @@ static void set_parameter_tlp(void * priv, const char * name, const bg_parameter
     gavl_dsp_context_set_quality(vp->ctx, val->val_i);
   }
 
-static void connect_input_port_tlp(void * priv,
-                                    bg_read_video_func_t func,
-                                    void * data, int stream, int port)
-  {
-  tlp_priv_t * vp;
-  vp = priv;
-
-  if(!port)
-    {
-    vp->read_func = func;
-    vp->read_data = data;
-    vp->read_stream = stream;
-    }
-  
-  }
-
-static void set_input_format_tlp(void * priv, gavl_video_format_t * format, int port)
-  {
-  tlp_priv_t * vp;
-  vp = priv;
-
-  gavl_video_format_copy(&vp->format, format);
-
-  if(vp->frame_1)
-    {
-    gavl_video_frame_destroy(vp->frame_1);
-    vp->frame_1 = NULL;
-    }
-  if(vp->frame_2)
-    {
-    gavl_video_frame_destroy(vp->frame_2);
-    vp->frame_2 = NULL;
-    }
-  vp->init = 1;
-  }
-
-static void get_output_format_tlp(void * priv, gavl_video_format_t * format)
-  {
-  tlp_priv_t * vp;
-  vp = priv;
-  gavl_video_format_copy(format, &vp->format);
-  }
-
 static void reset_tlp(void * priv)
   {
   tlp_priv_t * vp;
@@ -166,38 +129,93 @@ static void reset_tlp(void * priv)
   vp->init = 1;
   }
 
-static int read_video_tlp(void * priv, gavl_video_frame_t * frame, int stream)
+static gavl_source_status_t
+read_func(void * priv, gavl_video_frame_t ** frame)
   {
+  gavl_source_status_t st;
+  
   tlp_priv_t * vp;
   vp = priv;
   
-  if(!vp->frame_1)
-    vp->frame_1 = gavl_video_frame_create(&vp->format);
-  if(!vp->frame_2)
-    vp->frame_2 = gavl_video_frame_create(&vp->format);
+  if(!vp->frames[0])
+    vp->frames[0] = gavl_video_frame_create(&vp->format);
+  if(!vp->frames[1])
+    vp->frames[1] = gavl_video_frame_create(&vp->format);
   
   if(vp->init)
     {
-    if(!vp->read_func(vp->read_data, vp->frame_1, vp->read_stream))
-      return 0;
-    gavl_video_frame_copy(&vp->format, frame, vp->frame_1);
+    vp->this_frame = vp->frames[0];
+    vp->last_frame = vp->frames[1];
+
+    if((st = gavl_video_source_read_frame(vp->in_src, &vp->last_frame)) !=
+       GAVL_SOURCE_OK)
+      return st;
+    *frame = vp->last_frame;
     vp->init = 0;
     }
   else
     {
-    if(!vp->read_func(vp->read_data, vp->frame_2, vp->read_stream))
-      return 0;
+    gavl_video_frame_t * in_frame = NULL;
+    gavl_video_frame_t * tmp;
     
-    gavl_dsp_interpolate_video_frame(vp->ctx, &vp->format, vp->frame_1, vp->frame_2,
-                                     frame, vp->factor);
+    if((st = gavl_video_source_read_frame(vp->in_src, &in_frame)) !=
+       GAVL_SOURCE_OK)
+      return st;
     
-    gavl_video_frame_copy(&vp->format, vp->frame_1, frame);
+    gavl_dsp_interpolate_video_frame(vp->ctx, &vp->format, in_frame, vp->last_frame,
+                                     vp->this_frame, vp->factor);
+    *frame = vp->this_frame;
+    gavl_video_frame_copy_metadata(*frame, in_frame);
+
+    /* Swap */
+    tmp = vp->this_frame;
+    vp->this_frame = vp->last_frame;
+    vp->last_frame = tmp;
     }
-
-  gavl_video_frame_copy_metadata(frame, vp->frame_2);
-
-  return 1;
+  
+  return GAVL_SOURCE_OK;
   }
+
+static gavl_video_source_t *
+connect_tlp(void * priv, gavl_video_source_t * src,
+                 const gavl_video_options_t * opt)
+  {
+  tlp_priv_t * vp = priv;
+  
+  if(vp->out_src)
+    {
+    gavl_video_source_destroy(vp->out_src);
+    vp->out_src = NULL;
+    }
+  vp->in_src = src;
+
+  /* */
+  gavl_video_format_copy(&vp->format,
+                         gavl_video_source_get_src_format(vp->in_src));
+  
+  if(vp->frames[0])
+    {
+    gavl_video_frame_destroy(vp->frames[0]);
+    vp->frames[0] = NULL;
+    }
+  if(vp->frames[1])
+    {
+    gavl_video_frame_destroy(vp->frames[1]);
+    vp->frames[1] = NULL;
+    }
+  vp->init = 1;
+  
+  if(opt)
+    gavl_video_options_copy(gavl_video_source_get_options(vp->in_src), opt);
+  
+  gavl_video_source_set_dst(vp->in_src, 0, &vp->format);
+  vp->out_src =
+    gavl_video_source_create(read_func, vp,
+                             GAVL_SOURCE_SRC_ALLOC |
+                             GAVL_SOURCE_SRC_REF, &vp->format);
+  return vp->out_src;
+  }
+
 
 const bg_fv_plugin_t the_plugin = 
   {
@@ -216,12 +234,7 @@ const bg_fv_plugin_t the_plugin =
       .priority =         1,
     },
     
-    .connect_input_port = connect_input_port_tlp,
-    
-    .set_input_format = set_input_format_tlp,
-    .get_output_format = get_output_format_tlp,
-    
-    .read_video = read_video_tlp,
+    .connect = connect_tlp,
     .reset = reset_tlp,
   };
 
