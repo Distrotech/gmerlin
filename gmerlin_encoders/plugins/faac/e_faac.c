102c9c5a2155e0859efd4ab1a3e4384a6364e8d1
gmerlin <gmerlin@214d8ac5-8513-4f59-80f8-2f39b674a24a>
2012-11-29 19:37:24 +0000

    * Make e_faac use the codec module
    git-svn-id: svn://svn.code.sf.net/p/gmerlin/code/trunk@3513 214d8ac5-8513-4f59-80f8-2f39b674a24a

--- gmerlin_encoders/plugins/faac/e_faac.c
+++ gmerlin_encoders/plugins/faac/e_faac.c
@@ -36,6 +36,8 @@
 #include <gmerlin/log.h>
 #define LOG_DOMAIN "e_faac"
 
+#include "faac_codec.h"
+
 #include <gmerlin/translation.h>
 
 typedef struct
@@ -43,32 +45,24 @@ typedef struct
   FILE * output;
   char * filename;
   
-  faacEncHandle enc;
-  faacEncConfigurationPtr enc_config;
-  
   gavl_audio_format_t format;
-
-  gavl_audio_frame_t * frame;
-  uint8_t            * output_buffer;
-  unsigned int output_buffer_size;
-    
-  bgen_id3v1_t * id3v1;
   
+  bgen_id3v1_t * id3v1;
   int do_id3v1;
   int do_id3v2;
   int id3v2_charset;
-
-  int64_t samples_read;
   bg_encoder_callbacks_t * cb;
-
   gavl_audio_sink_t * sink;
+  gavl_packet_sink_t * psink;
   
+  bg_faac_t * codec;
   } faac_t;
 
 static void * create_faac()
   {
   faac_t * ret;
   ret = calloc(1, sizeof(*ret));
+  ret->codec = bg_faac_create();
   return ret;
   }
 
@@ -86,85 +80,10 @@ static void set_callbacks_faac(void * data, bg_encoder_callbacks_t * cb)
   faac->cb = cb;
   }
 
-static const bg_parameter_info_t audio_parameters[] =
-  {
-    {
-      .name =        "basic",
-      .long_name =   TRS("Basic options"),
-      .type =        BG_PARAMETER_SECTION
-    },
-    {
-      .name =        "object_type",
-      .long_name =   TRS("Object type"),
-      .type =        BG_PARAMETER_STRINGLIST,
-      .val_default = { .val_str =  "mpeg4_main" },
-      .multi_names = (char const *[]){ "mpeg2_main",
-                              "mpeg2_lc",
-                              "mpeg4_main",
-                              "mpeg4_lc",
-                              "mpeg4_ltp",
-                              NULL },
-      .multi_labels = (char const *[]){ TRS("MPEG-2 Main profile"),
-                               TRS("MPEG-2 Low Complexity profile (LC)"),
-                               TRS("MPEG-4 Main profile"),
-                               TRS("MPEG-4 Low Complexity profile (LC)"),
-                               TRS("MPEG-4 Long Term Prediction (LTP)"),
-                               NULL },
-    },
-    {
-      .name =        "bitrate",
-      .long_name =   TRS("Bitrate (kbps)"),
-      .type =        BG_PARAMETER_INT,
-      .val_min =     { .val_i = 0    },
-      .val_max =     { .val_i = 1000 },
-      .help_string = TRS("Average bitrate (0: VBR based on quality)"),
-    },
-    {
-      .name =        "quality",
-      .long_name =   TRS("Quality"),
-      .type =        BG_PARAMETER_SLIDER_INT,
-      .val_min =     { .val_i = 10 },
-      .val_max =     { .val_i = 500 },
-      .val_default = { .val_i = 100 },
-      .help_string = TRS("Quantizer quality"),
-    },
-    {
-      .name =        "advanced",
-      .long_name =   TRS("Advanced options"),
-      .type =        BG_PARAMETER_SECTION
-    },
-    {
-      .name =        "block_types",
-      .long_name =   TRS("Block types"),
-      .type =        BG_PARAMETER_STRINGLIST,
-      .val_default = { .val_str =  "Both" },
-      .multi_names = (char const *[]){ "Both",
-                              "No short",
-                              "No long",
-                              NULL },
-      .multi_labels = (char const *[]){ TRS("Both"),
-                               TRS("No short"),
-                               TRS("No long"),
-                               NULL },
-    },
-    {
-      .name =        "tns",
-      .type =        BG_PARAMETER_CHECKBUTTON,
-      .long_name =   TRS("Use temporal noise shaping"),
-      .val_default = { .val_i = 0 }
-    },
-    {
-      .name =        "no_midside",
-      .long_name =   TRS("Don\'t use mid/side coding"),
-      .type =        BG_PARAMETER_CHECKBUTTON,
-      .val_default = { .val_i = 0 }
-    },
-    { /* End of parameters */ }
-  };
-
 static const bg_parameter_info_t * get_audio_parameters_faac(void * data)
   {
-  return audio_parameters;
+  faac_t * faac = data;
+  return bg_faac_get_parameters(faac->codec);
   }
 
 static void set_audio_parameter_faac(void * data, int stream, const char * name,
@@ -172,78 +91,10 @@ static void set_audio_parameter_faac(void * data, int stream, const char * name,
   {
   faac_t * faac;
   faac = data;
-  
   if(stream)
     return;
-    
-  else if(!name)
-    {
-    /* Set encoding parameters */
-    
-    if(!faacEncSetConfiguration(faac->enc, faac->enc_config))
-      bg_log(BG_LOG_ERROR, LOG_DOMAIN,  "faacEncSetConfiguration failed");
-    }
-    
-  else if(!strcmp(name, "object_type"))
-    {
-    if(!strcmp(v->val_str, "mpeg2_main"))
-      {
-      faac->enc_config->mpegVersion = MPEG2;
-      faac->enc_config->aacObjectType = MAIN;
-      }
-    else if(!strcmp(v->val_str, "mpeg2_lc"))
-      {
-      faac->enc_config->mpegVersion = MPEG2;
-      faac->enc_config->aacObjectType = LOW;
-      }
-    else if(!strcmp(v->val_str, "mpeg4_main"))
-      {
-      faac->enc_config->mpegVersion = MPEG4;
-      faac->enc_config->aacObjectType = MAIN;
-      }
-    else if(!strcmp(v->val_str, "mpeg4_lc"))
-      {
-      faac->enc_config->mpegVersion = MPEG4;
-      faac->enc_config->aacObjectType = LOW;
-      }
-    else if(!strcmp(v->val_str, "mpeg4_ltp"))
-      {
-      faac->enc_config->mpegVersion = MPEG4;
-      faac->enc_config->aacObjectType = LTP;
-      }
-    }
-  else if(!strcmp(name, "bitrate"))
-    {
-    faac->enc_config->bitRate =
-      (v->val_i * 1000) / faac->format.num_channels;
-    }
-  else if(!strcmp(name, "quality"))
-    {
-    faac->enc_config->quantqual = v->val_i;
-    }
-  else if(!strcmp(name, "block_types"))
-    {
-    if(!strcmp(v->val_str, "Both"))
-      {
-      faac->enc_config->shortctl = SHORTCTL_NORMAL;
-      }
-    else if(!strcmp(v->val_str, "No short"))
-      {
-      faac->enc_config->shortctl = SHORTCTL_NOSHORT;
-      }
-    else if(!strcmp(v->val_str, "No long"))
-      {
-      faac->enc_config->shortctl = SHORTCTL_NOLONG;
-      }
-    }
-  else if(!strcmp(name, "tns"))
-    {
-    faac->enc_config->useTns = v->val_i;
-    }
-  else if(!strcmp(name, "no_midside"))
-    {
-    faac->enc_config->allowMidside = !v->val_i;
-    }
+
+  bg_faac_set_parameter(faac->codec, name, v);
   }
 
 static const bg_parameter_info_t parameters[] =
@@ -318,7 +169,6 @@ static int open_faac(void * data, const char * filename,
     return 0;
     }
   
-
   if(faac->do_id3v1 && metadata)
     faac->id3v1 = bgen_id3v1_create(metadata);
   if(faac->do_id3v2 && metadata)
@@ -330,80 +180,18 @@ static int open_faac(void * data, const char * filename,
   return 1;
   }
 
-static int flush_audio(faac_t * faac)
+static gavl_sink_status_t write_packet(void * data, gavl_packet_t * p)
   {
-  int i, imax;
-  int bytes_encoded;
-
-  /* First, we must scale the samples to -32767 .. 32767 */
-
-  imax = faac->frame->valid_samples * faac->format.num_channels;
-  
-  for(i = 0; i < imax; i++)
-    faac->frame->samples.f[i] *= 32767.0;
-  
-  /* Encode the stuff */
-
-  bytes_encoded = faacEncEncode(faac->enc,
-                                (int32_t*)faac->frame->samples.f,
-                                faac->frame->valid_samples * faac->format.num_channels,
-                                faac->output_buffer, faac->output_buffer_size);
-  faac->frame->valid_samples = 0;
-
-  /* Write this to the file */
-
-  if(bytes_encoded)
-    {
-    if(fwrite(faac->output_buffer, 1, bytes_encoded, faac->output) < bytes_encoded)
-      return -1;
-    }
-  return bytes_encoded;
-  }
-
-static gavl_sink_status_t
-write_audio_func_faac(void * data, gavl_audio_frame_t * frame)
-  {
-  int samples_done = 0;
-  int samples_copied;
-  
-  faac_t * faac;
-  faac = data;
-  
-  while(samples_done < frame->valid_samples)
-    {
-
-    /* Copy frame into our buffer */
-    
-    samples_copied =
-      gavl_audio_frame_copy(&faac->format,
-                            faac->frame,                                                 /* dst */
-                            frame,                                                       /* src */
-                            faac->frame->valid_samples,                                  /* dst_pos */
-                            samples_done,                                                /* src_pos */
-                            faac->format.samples_per_frame - faac->frame->valid_samples, /* dst_size */
-                            frame->valid_samples - samples_done);                        /* src_size */
-    
-    samples_done += samples_copied;
-    faac->frame->valid_samples += samples_copied;
-    
-    /* Encode buffer */
-
-    if(faac->frame->valid_samples == faac->format.samples_per_frame)
-      {
-      if(flush_audio(faac) < 0)
-        return GAVL_SINK_ERROR;
-      }
-    }
-  
-  faac->samples_read += frame->valid_samples;
+  faac_t * faac = data;
+  if(fwrite(p->data, 1, p->data_len, faac->output) < p->data_len)
+    return GAVL_SINK_ERROR;
   return GAVL_SINK_OK;
   }
 
 static int write_audio_frame_faac(void * data, gavl_audio_frame_t * frame,
                                   int stream)
   {
-  faac_t * faac;
-  faac = data;
+  faac_t * faac = data;
   return (gavl_audio_sink_put_frame(faac->sink, frame) == GAVL_SINK_OK);
   }
 
@@ -411,80 +199,25 @@ static int add_audio_stream_faac(void * data,
                                  const gavl_metadata_t * m,
                                  const gavl_audio_format_t * format)
   {
-  unsigned long input_samples;
-  unsigned long output_bytes;
-  
-  faac_t * faac;
-  
-  faac = data;
-
-  /* Create encoder handle and get configuration */
-
-  faac->enc = faacEncOpen(format->samplerate,
-                          format->num_channels,
-                          &input_samples,
-                          &output_bytes);
-  
-  faac->enc_config = faacEncGetCurrentConfiguration(faac->enc);
-  faac->enc_config->inputFormat = FAAC_INPUT_FLOAT;
-  
-  /* Copy and adjust format */
-
+  faac_t * faac = data;
   gavl_audio_format_copy(&faac->format, format);
-
-  faac->format.interleave_mode = GAVL_INTERLEAVE_ALL;
-  faac->format.sample_format   = GAVL_SAMPLE_FLOAT;
-  faac->format.samples_per_frame = input_samples / format->num_channels;
-
-  switch(faac->format.num_channels)
-    {
-    case 1:
-      faac->format.channel_locations[0] = GAVL_CHID_FRONT_CENTER;
-      break;
-    case 2:
-      faac->format.channel_locations[0] = GAVL_CHID_FRONT_LEFT;
-      faac->format.channel_locations[1] = GAVL_CHID_FRONT_RIGHT;
-      break;
-    case 3:
-      faac->format.channel_locations[0] = GAVL_CHID_FRONT_CENTER;
-      faac->format.channel_locations[1] = GAVL_CHID_FRONT_LEFT;
-      faac->format.channel_locations[2] = GAVL_CHID_FRONT_RIGHT;
-      break;
-    case 4:
-      faac->format.channel_locations[0] = GAVL_CHID_FRONT_CENTER;
-      faac->format.channel_locations[1] = GAVL_CHID_FRONT_LEFT;
-      faac->format.channel_locations[2] = GAVL_CHID_FRONT_RIGHT;
-      faac->format.channel_locations[3] = GAVL_CHID_REAR_CENTER;
-      break;
-    case 5:
-      faac->format.channel_locations[0] = GAVL_CHID_FRONT_CENTER;
-      faac->format.channel_locations[1] = GAVL_CHID_FRONT_LEFT;
-      faac->format.channel_locations[2] = GAVL_CHID_FRONT_RIGHT;
-      faac->format.channel_locations[3] = GAVL_CHID_REAR_LEFT;
-      faac->format.channel_locations[4] = GAVL_CHID_REAR_RIGHT;
-      break;
-    case 6:
-      faac->format.channel_locations[0] = GAVL_CHID_FRONT_CENTER;
-      faac->format.channel_locations[1] = GAVL_CHID_FRONT_LEFT;
-      faac->format.channel_locations[2] = GAVL_CHID_FRONT_RIGHT;
-      faac->format.channel_locations[3] = GAVL_CHID_REAR_LEFT;
-      faac->format.channel_locations[4] = GAVL_CHID_REAR_RIGHT;
-      faac->format.channel_locations[5] = GAVL_CHID_LFE;
-      break;
-
-    }
-  
-  faac->frame = gavl_audio_frame_create(&faac->format);
-  
-  faac->output_buffer = malloc(output_bytes);
-  faac->output_buffer_size = output_bytes;
-
-  faac->sink =
-    gavl_audio_sink_create(NULL, write_audio_func_faac, faac, &faac->format);
-  
   return 0;
   }
 
+static int start_faac(void * data)
+  {
+  faac_t * faac = data;
+  faac->sink = bg_faac_open(faac->codec,
+                            NULL,
+                            &faac->format,
+                            NULL);
+  if(!faac->sink)
+    return 0;
+
+  faac->psink = gavl_packet_sink_create(NULL, write_packet, faac);
+  bg_faac_set_packet_sink(faac->codec, faac->psink);
+  return 1;
+  }
 
 static void get_audio_format_faac(void * data, int stream,
                                  gavl_audio_format_t * ret)
@@ -502,30 +235,14 @@ static gavl_audio_sink_t * get_audio_sink_faac(void * data, int stream)
 
 static int close_faac(void * data, int do_delete)
   {
-  int ret = 1, result;
+  int ret = 1;
   faac_t * faac;
   faac = data;
-
-  /* Flush remaining audio data */
-
-  if(faac->samples_read)
-    {
-    while(1)
-      {
-      result = flush_audio(faac);
-      if(result > 0)
-        continue;
-      else if(result < 0)
-        ret = 0;
-      break;
-      }
-    }
   
-  if(faac->enc)
-    {
-    faacEncClose(faac->enc);
-    faac->enc = NULL;
-    }
+  /* Destroy codec, will also flush samples */
+  
+  bg_faac_destroy(faac->codec);
+  faac->codec = NULL;
   
   if(faac->output)
     {
@@ -541,10 +258,10 @@ static int close_faac(void * data, int do_delete)
     faac->output = NULL;
     }
 
-  if(faac->sink)
+  if(faac->psink)
     {
-    gavl_audio_sink_destroy(faac->sink);
-    faac->sink = NULL;
+    gavl_packet_sink_destroy(faac->psink);
+    faac->psink = NULL;
     }
   if(faac->filename)
     {
@@ -584,6 +301,7 @@ const bg_encoder_plugin_t the_plugin =
     .add_audio_stream =        add_audio_stream_faac,
     
     .set_audio_parameter =     set_audio_parameter_faac,
+    .start               =     start_faac,
 
     .get_audio_format =        get_audio_format_faac,
     .get_audio_sink =        get_audio_sink_faac,
