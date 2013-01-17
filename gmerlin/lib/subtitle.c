0276c43d3940600acceb18c419840eafd3eda6cd
gmerlin <gmerlin@214d8ac5-8513-4f59-80f8-2f39b674a24a>
2013-01-17 18:56:11 +0000

    * Update
    git-svn-id: svn://svn.code.sf.net/p/gmerlin/code/trunk@3646 214d8ac5-8513-4f59-80f8-2f39b674a24a

--- gmerlin/lib/subtitle.c
+++ gmerlin/lib/subtitle.c
@@ -22,15 +22,20 @@
 #include <config.h>
 
 #include <stdlib.h>
+#include <string.h>
 
 #include <gavl/gavl.h>
 #include <gavl/connectors.h>
 
 #include <gmerlin/subtitle.h>
+#include <gmerlin/log.h>
+#define LOG_DOMAIN "subtitle"
+
 
 struct bg_subtitle_handler_s
   {
   gavl_video_format_t video_format;
+  gavl_video_format_t ovl_format;
   
   gavl_video_source_t * src;
   gavl_video_sink_t   * sink;
@@ -39,18 +44,24 @@ struct bg_subtitle_handler_s
   gavl_video_frame_t * next;
 
   gavl_video_frame_t * ovl[2];
-  
-  };
 
+  gavl_video_frame_t * out_ovl;
+  
+  int eof;
+  int active; // Current subtitle is active
+  };
 bg_subtitle_handler_t * bg_subtitle_handler_create()
   {
   bg_subtitle_handler_t * ret;
   ret = calloc(1, sizeof(*ret));
+  ret->out_ovl = gavl_video_frame_create(NULL);
   return ret;
   }
 
 void bg_subtitle_handler_destroy(bg_subtitle_handler_t * h)
   {
+  gavl_video_frame_null(h->out_ovl);
+  gavl_video_frame_destroy(h->out_ovl);
   free(h);
   }
 
@@ -59,7 +70,6 @@ void bg_subtitle_handler_init(bg_subtitle_handler_t * h,
                               gavl_video_source_t * src,
                               gavl_video_sink_t * sink)
   {
-  const gavl_video_format_t * ovl_format;
   
   h->src = src;
   h->sink = sink;
@@ -67,29 +77,121 @@ void bg_subtitle_handler_init(bg_subtitle_handler_t * h,
   if(!h->src || !h->sink)
     return;
 
-  ovl_format = gavl_video_sink_get_format(h->sink);
+  gavl_video_format_copy(&h->ovl_format,
+                         gavl_video_sink_get_format(h->sink));
   
-  gavl_video_source_set_dst(h->src, 0, ovl_format);
+  gavl_video_source_set_dst(h->src, 0, &h->ovl_format);
   
   gavl_video_format_copy(&h->video_format, video_format);
 
-  h->ovl[0] = gavl_video_frame_create(ovl_format);
-  h->ovl[1] = gavl_video_frame_create(ovl_format);
+  h->ovl[0] = gavl_video_frame_create(&h->ovl_format);
+  h->ovl[1] = gavl_video_frame_create(&h->ovl_format);
   h->cur = h->ovl[0];
   h->next = h->ovl[1];
   }
 
+#ifdef DUMP_SUBTITLE
+static void dump_subtitle(gavl_overlay_t * ovl)
+  {
+  bg_dprintf("Got subtitle %f -> %f (%f) ",
+             gavl_time_to_seconds(ovl->timestamp),
+             gavl_time_to_seconds(ovl->timestamp +
+                                  ovl->duration),
+             gavl_time_to_seconds(ovl->duration));
+  
+  gavl_rectangle_i_dump(&ovl->src_rect);
+  bg_dprintf(" dst: %d %d\n", ovl->dst_x, ovl->dst_y);
+  }
+#endif
+static void read_subtitle(bg_subtitle_handler_t * h,
+                          gavl_video_frame_t * frame)
+  {
+  gavl_source_status_t st;
+  if((frame->src_rect.w > 0) || h->eof)
+    return;
+  
+  st = gavl_video_source_read_frame(h->src, &frame);
+  if(st == GAVL_SOURCE_EOF)
+    {
+    h->eof = 1;
+    bg_log(BG_LOG_INFO, LOG_DOMAIN, "Subtitle stream finished");
+    }
+  if(st == GAVL_SOURCE_OK)
+    {
+#ifdef DUMP_SUBTITLE
+    dump_subtitle(frame);
+#endif
+    }
+  }
+
+static void put_overlay(bg_subtitle_handler_t * h)
+  {
+  h->active = 1;
+  memcpy(h->out_ovl, h->cur, sizeof(*h->cur));
+  gavl_video_sink_put_frame(h->sink, h->out_ovl);
+  }
+
+static void clear_overlay(bg_subtitle_handler_t * h)
+  {
+  h->active = 0;
+  h->out_ovl->src_rect.w = 0;
+  gavl_video_sink_put_frame(h->sink, h->out_ovl);
+  }
 void bg_subtitle_handler_update(bg_subtitle_handler_t * h,
                                 const gavl_video_frame_t * frame)
   {
-  gavl_source_status_t st;
+  gavl_video_frame_t * swp;
+  gavl_time_t frame_start;
+  gavl_time_t frame_end;
+  gavl_time_t overlay_start;
+  gavl_time_t overlay_end;
+  
+  frame_start = gavl_time_unscale(h->video_format.timescale, frame->timestamp);
+  frame_end = gavl_time_unscale(h->video_format.timescale, frame->timestamp + frame->duration);
+  
   /* Check if the subtitle is expired */
-
-  if(h->cur->src_rect.w)
+  if(h->active && (h->cur->duration > 0))
     {
-    if(gavl_time_unscale(
+    overlay_end = gavl_time_unscale(h->ovl_format.timescale,
+                                    h->cur->timestamp + h->cur->duration);
+    if(frame_start > overlay_end)
+      {
+      clear_overlay(h);
+      
+      /* Swap */
+      swp = h->cur;
+      h->cur = h->next;
+      h->next = swp;
+      }
     }
-       
+
+  /* Read as many subtitles as possible */
+  read_subtitle(h, h->cur);
+  read_subtitle(h, h->next);
+
+  /* Check if the current subtitle became valid */
+  if(!h->active && h->cur->src_rect.w)
+    {
+    overlay_start = gavl_time_unscale(h->ovl_format.timescale,
+                                      h->cur->timestamp);
+
+    if(overlay_start < frame_end)
+      put_overlay(h);
+    }
+  /* Check if the next subtitle became valid */
+  else if(h->next->src_rect.w)
+    {
+    overlay_start = gavl_time_unscale(h->ovl_format.timescale,
+                                      h->next->timestamp);
+    if(overlay_start < frame_end)
+      {
+      put_overlay(h);
+      /* Swap */
+      swp = h->cur;
+      h->cur = h->next;
+      h->next = swp;
+      }
+    }
   }
 
 void bg_subtitle_handler_reset(bg_subtitle_handler_t * h)
